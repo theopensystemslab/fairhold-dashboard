1d1ca64be77cfa0a2bce136c391d9f0c5b076e25
Gabriele Granello <gabriele@opensystemslab.io>
2024-02-16 13:44:28 +0100

    Fixed the API and input management: now form data are passed to the API

--- app/components/ui/CalculatorInput.tsx
+++ app/components/ui/CalculatorInput.tsx
@@ -1,18 +1,33 @@
 "use client";
+import { trueDependencies } from "mathjs";
 import React, { useState } from "react";
 
 const CalculatorInput = () => {
-  const [housePostcode, sethousePostcode] = useState("");
-  const [houseType, sethouseType] = useState("");
-  const [houseBeds, setHouseBeds] = useState("");
-  const [howSize, setHouseSize] = useState("");
-  const [houseAge, setHouseAge] = useState("");
+  const [housePostcode, sethousePostcode] = useState(""); // variable associated to the postcode
+  const houseTypes = {
+    Detached: "D",
+    Semidetached: "S",
+    Terrace: "T",
+    Flat: "F",
+  }; // variables associated to the house types
+  const [houseType, setHouseType] = useState(houseTypes.Detached); // variables associated to the house type
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
+  const [houseBedrooms, setHouseBedrooms] = useState(""); // variables associated to the number of bedrooms in the house
+  const [howSize, setHouseSize] = useState(""); // variables associated to the house size
+  const [houseAge, setHouseAge] = useState(""); // variables associated to the house age
 
-    //fetch the data
-    fetch("/api") // Replace 'your-api-route' with the actual path to your API route
+  // fucntion that defines what happens after submitting the form
+  async function handleSubmit(e: any) {
+    e.preventDefault(); // pr event the default of the form
+    const formData = new FormData(e.currentTarget); // get the data in the form, e.g postcode, house size etc
+
+    //fetch the data: call the api and attach the form data
+    const response = await fetch("/api", {
+      method: "POST",
+      body: formData,
+    });
+    console.log(response);
+    /*     fetch("/api") // Replace 'your-api-route' with the actual path to your API route
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
@@ -26,10 +41,10 @@ const CalculatorInput = () => {
       .catch((error) => {
         // Handle any errors here
         console.error("Error:", error);
-      });
+      }); */
 
-    console.log(housePostcode);
-  };
+    //console.log(housePostcode);
+  }
 
   return (
     <div className="flex items-center justify-center text-black mt-5">
@@ -47,6 +62,7 @@ const CalculatorInput = () => {
             placeholder="e.g. SE17 1PE"
             value={housePostcode}
             onChange={(e) => sethousePostcode(e.target.value)}
+            name="housePostcode"
           />
           <h2 className="mb-1 font-bold">House size</h2>
           <input
@@ -57,7 +73,62 @@ const CalculatorInput = () => {
             placeholder="Provide the house size in m square, e.g. 66"
             value={howSize}
             onChange={(e) => setHouseSize(e.target.value)}
+            name="houseSize"
           />
+          <h2 className="mb-1 font-bold">House typology</h2>
+          <div className="flex">
+            <label className="mx-2">
+              <input
+                className="accent-black"
+                type="radio"
+                id="Detached"
+                name="houseType"
+                value={houseTypes.Detached}
+                checked={houseType === houseTypes.Detached}
+                onChange={() => setHouseType(houseTypes.Detached)}
+              />
+              Detached
+            </label>
+
+            <label className="mx-2">
+              <input
+                className="accent-black"
+                type="radio"
+                id="Semidetached"
+                name="houseType"
+                value={houseTypes.Semidetached}
+                checked={houseType === houseTypes.Semidetached}
+                onChange={() => setHouseType(houseTypes.Semidetached)}
+              />
+              Semi-Detached
+            </label>
+
+            <label className="mx-2">
+              <input
+                className="accent-black"
+                type="radio"
+                id="Terrace"
+                name="houseType"
+                value={houseTypes.Terrace}
+                checked={houseType === houseTypes.Terrace}
+                onChange={() => setHouseType(houseTypes.Terrace)}
+              />
+              Terrace
+            </label>
+
+            <label className="mx-2">
+              <input
+                className="accent-black"
+                type="radio"
+                id="Flat"
+                name="houseType"
+                value={houseTypes.Flat}
+                checked={houseType === houseTypes.Flat}
+                onChange={() => setHouseType(houseTypes.Flat)}
+              />
+              Flat
+            </label>
+          </div>
 
           <h2 className="mb-1 font-bold">House age</h2>
           <input
@@ -68,8 +139,8 @@ const CalculatorInput = () => {
             placeholder="Provide the house age in years. For a new build, insert age 1"
             value={houseAge}
             onChange={(e) => setHouseAge(e.target.value)}
+            name="houseAge"
           />
-
           <h2 className="mb-1 font-bold">Number of bedrooms</h2>
           <input
             className="mb-3 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
@@ -77,10 +148,10 @@ const CalculatorInput = () => {
              rounded-md "
             type="integer"
             placeholder="Provide the number of bedrooms e.g. 2"
-            value={houseBeds}
-            onChange={(e) => setHouseBeds(e.target.value)}
+            value={houseBedrooms}
+            onChange={(e) => setHouseBedrooms(e.target.value)}
+            name="houseBedrooms"
           />
-
           <button
             className="text-white bg-black w-1/3 rounded-xl"
             type="submit"
--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "***REMOVED***",
-    database: "fairhold",
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
