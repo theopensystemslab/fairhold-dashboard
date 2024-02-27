2cffa6aa02235d59014de06b9aff8e023913fbbd
Gabriele Granello <gabriele@opensystemslab.io>
2024-02-27 10:43:43 +0100

    Linked the API with the classes

--- sharedCode/testClasses.ts
+++ sharedCode/testClasses.ts
@@ -1,4 +1,19 @@
 import { Mortgage, House, Fairhold } from "./classes";
 
-const fairhold = Fairhold; // set the fairhold
-console.log(fairhold);
+const fairhold = new Fairhold(); // set the fairhold
+const mortgage = new Mortgage(200000); // set the new mortgage
+
+function calculateFairhold(houseData: any) {
+  console.log(houseData.buildPrice.priceMid);
+  const house = new House(
+    houseData.granularityPostcode,
+    houseData.houseType,
+    houseData.houseBedrooms,
+    houseData.houseAge,
+    houseData.houseSize,
+    houseData.buildPrice[0].priceMid
+  );
+  return console.log(house);
+}
+
+export default calculateFairhold;
