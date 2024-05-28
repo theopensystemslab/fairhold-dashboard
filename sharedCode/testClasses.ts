0d63e5fefb0b802536854e5f7f3efb9a0f9da1f8
zzhhaa <zzhhaa@protonmail.com>
2024-05-28 11:59:05 +0100

    Added error handling to calculateFairhold function

--- sharedCode/testClasses.ts
+++ sharedCode/testClasses.ts
@@ -1,14 +1,15 @@
 import { Mortgage, Property, Fairhold, Household } from "./classes";
 
 // ADDED ERROR HANDLING BEFORE ---------------------------------------------------------------------------------------
-// if (!houseData.buildPrice || houseData.buildPrice.length === 0) {
-//   throw new Error("buildPrice data is missing or empty");
-// }
-// if (!houseData.itl3 || houseData.itl3.length === 0) {
-//   throw new Error("itl3 data is missing or empty");
-// }
+
 
 function calculateFairhold(responseData: any) {
+if (!responseData.buildPrice || responseData.buildPrice.length === 0) {
+  throw new Error("buildPrice data is missing or empty");
+}
+if (!responseData.itl3 || responseData.itl3.length === 0) {
+  throw new Error("itl3 data is missing or empty");
+}
 
   // define the property object
   const property = new Property(
