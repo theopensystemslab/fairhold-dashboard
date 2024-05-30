629bb04b2e613e150296d0c564dd92c419bd3b82
zzhhaa <zzhhaa@protonmail.com>
2024-05-30 14:38:15 +0100

    Updated object to match fetched data structure

--- sharedCode/testClasses.ts
+++ sharedCode/testClasses.ts
@@ -1,8 +1,5 @@
 import { Mortgage, Property, Fairhold, Household } from "./classes";
 
-// ADDED ERROR HANDLING BEFORE ---------------------------------------------------------------------------------------
-
-
 function calculateFairhold(responseData: any) {
 if (!responseData.buildPrice || responseData.buildPrice.length === 0) {
   throw new Error("buildPrice data is missing or empty");
@@ -13,20 +10,20 @@ if (!responseData.itl3 || responseData.itl3.length === 0) {
 
   // define the property object
   const property = new Property(
-    houseData.postcode,
-    houseData.houseType,
-    houseData.houseBedrooms,
-    houseData.houseAge,
-    houseData.houseSize,
-    houseData.buildPrice[0].priceMid,
-    houseData.averagePrice,
-    houseData.itl3[0].itl3
+    responseData.postcode,
+    responseData.houseType,
+    responseData.houseBedrooms,
+    responseData.houseAge,
+    responseData.houseSize,
+    responseData.buildPrice,
+    responseData.averagePrice,
+    responseData.itl3
   );
   const household = new Household(
-    houseData.gdhi[0].gdhi_2020,
-    houseData.averageRent,
-    houseData.averageSocialRent,
-    houseData.hpi
+    responseData.gdhi,
+    responseData.averageRent,
+    responseData.averageSocialRent,
+    responseData.hpi
   );
   return console.log(property, household);
 }
