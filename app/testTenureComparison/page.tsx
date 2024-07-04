6ca0e63ef621d6ca248b76bfda37c3164629e95e
zzhhaa <zzhhaa@protonmail.com>
2024-07-04 11:05:50 +0100

    Re-order tenures to put Fairhold as last two

--- app/testTenureComparison/page.tsx
+++ app/testTenureComparison/page.tsx
@@ -9,17 +9,17 @@ const TestTenureComparison = () => {
         category: 'Monthly Costs Land',
         marketPurchase: 1200,
         marketRent: 1000,
+        socialRent: 200,
         fairholdPurchase: 800,
         fairholdRent: 700,
-        socialRent: 200,
       },
       {
         category: 'Monthly Costs House',
         marketPurchase: 500,
         marketRent: 400,
+        socialRent: 100,
         fairholdPurchase: 500,
         fairholdRent: 500,
-        socialRent: 100,
       },
     ];
   
