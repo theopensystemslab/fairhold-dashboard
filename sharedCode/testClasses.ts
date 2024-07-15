746c44227e1e12a7fa051a8a51f10bdec1907a4c
zzhhaa <zzhhaa@protonmail.com>
2024-07-15 16:56:41 +0100

    Fixed socialRent keys in Household object

--- sharedCode/testClasses.ts
+++ sharedCode/testClasses.ts
@@ -24,7 +24,7 @@ function calculateFairhold(responseData: any) {
   const household = new Household({
     incomePerPerson: responseData.gdhi,
     averageRent: responseData.averageRent,
-    socialRentAveEarning: responseData.averageSocialRent,
+    socialRentAveEarning: responseData.socialRentAveEarning,
     socialRentAdjustments: responseData.socialRentAdjustments,
     housePriceIndex: responseData.hpi,
     gasBillYearly: responseData.gasBillYearly,
