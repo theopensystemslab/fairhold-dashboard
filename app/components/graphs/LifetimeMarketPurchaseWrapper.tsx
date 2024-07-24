2142df7b4c51499381ce4585f14ad7e0a70fa968
zzhhaa <zzhhaa@protonmail.com>
2024-07-24 15:22:54 +0100

    Refactored wrapper to take new data structure

--- app/components/graphs/LifetimeMarketPurchaseWrapper.tsx
+++ app/components/graphs/LifetimeMarketPurchaseWrapper.tsx
@@ -1,39 +1,37 @@
 "use client";
 import React from 'react';
-import { Household } from '@/sharedCode/classes';
+import { Household, lifetimeTypes } from '@/sharedCode/classes';
 import LifetimeCombinedChart from './LifetimeCombinedChart';
 
-interface Forecast {
-    forecastMarket: {
-      year: number;
-      landPrice: number;
-      newBuildPrice: number;
-      maintenanceCost: number;
-      gasBillYearly: number;
-      affordableIncomeYearly: number;
-    };
-  }
-  interface LifetimeCombinedWrapperProps {
-    forecast: Forecast;
-    colorScheme: {
-      land: string;
-      house: string;
-      maintenance: string;
-      bills: string;
-      incomeThreshold: string;
-    };
+interface LifetimeMarketPurchaseWrapperProps {
+    household: Household;
   }
 
-  const LifetimeCombinedWrapper: React.FC<LifetimeCombinedWrapperProps> = ({ forecast, colorScheme }) => {
+const LifetimeMarketPurchaseWrapper: React.FC<LifetimeMarketPurchaseWrapperProps> = ({ household }) => {
+  console.log('LifetimeMarketPurchaseWrapper household: ', household)
+
+  // Create color scheme for different versions of the graph
+    const colorScheme = {
+      land: '#194285',
+      house: '#5178b8',
+      maintenance: '#595959',
+      bills: '#adadad',
+      incomeThreshold: '#6c9e6e'
+      }
+
+    console.log('household.tenure.marketPurchase.lifetime?.lifetimeMarket', household.tenure.marketPurchase?.lifetime)
+
     // Process and format the data for the chart
-    const chartData = forecast.forecastMarket.map(item => ({
-      year: item.year.toString(),
-      landCost: item.landPrice,
-      houseCost: item.newBuildPrice,
+    const chartData = ((household.tenure.marketPurchase?.lifetime as lifetimeTypes[])?.map((item, index) => ({
+      year: index.toString(), // or just `year: index` if you want it as a number
+      landCost: item.landMortgagePaymentYearly,
+      houseCost: item.houseMortgagePaymentYearly,
       maintenanceCost: item.maintenanceCost,
       billsCost: item.gasBillYearly,
-      incomeThreshold: item.affordableIncomeYearly,
-    }));
+      incomeThreshold: item.affordabilityThresholdIncome,
+    })) ?? []);
+
+    console.log('LifetimeMarketPurchaseWrapper.tsx chartData: ', chartData)
 
     return (
       <div>
