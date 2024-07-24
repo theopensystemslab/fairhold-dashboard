718982478ea176067d73676a1d4307bc1841dc2b
zzhhaa <zzhhaa@protonmail.com>
2024-07-24 11:25:02 +0100

    Refactored using new data structure

--- app/components/graphs/TenureComparisonWrapper.tsx
+++ app/components/graphs/TenureComparisonWrapper.tsx
@@ -4,16 +4,16 @@ import { Household } from "@/sharedCode/classes";
 import TenureComparisonBarChart from "./TenureComparisonBarChart";
 
 interface TenureComparisonWrapperProps {
-  household?: Household;
-  mortgageLand?: { monthlyPayment: number };
-  averageRentLand?: { averageRentLandMonthly: number };
-  socialRentMonthlyLand?: { socialRentMonthlyLand: number };
-  mortgageFairholdLandPurchase?: { monthlyPayment: number };
-  fairholdLandRent?: { discountedLandRent: number };
-  mortgageHouse?: { monthlyPayment: number };
-  mortgageDepreciatedHouse?: { monthlyPayment: number };
-  averageRentHouse?: { averageRentHouse: number };
-  socialRentMonthlyHouse?: { socialRentMonthlyHouse: number };
+  household: Household;
+  mortgageLand?: number;
+  averageRentLand?: number;
+  socialRentMonthlyLand?: number;
+  mortgageFairholdLandPurchase?: number;
+  fairholdLandRent?: number;
+  mortgageHouse?: number;
+  mortgageDepreciatedHouse?: number;
+  averageRentHouse?: number;
+  socialRentMonthlyHouse?: number;
 }
 
 const TenureComparisonWrapper: React.FC<TenureComparisonWrapperProps> = ({
@@ -25,94 +25,28 @@ const TenureComparisonWrapper: React.FC<TenureComparisonWrapperProps> = ({
     return <div>No household data available</div>;
   }
 
-  const transformHouseholdToProps = (
-    household: Household
-  ): TenureComparisonWrapperProps => {
-    return {
-      household,
-      mortgageLand: household.tenure.marketPurchase?.landMortgage
-        ? {
-            monthlyPayment:
-              household.tenure.marketPurchase.landMortgage.monthlyPayment || 0,
-          }
-        : undefined,
-      averageRentLand: household.tenure.marketRent?.averageRentLandYearly
-        ? { averageRentLandMonthly: household.tenure.marketRent.averageRentLandYearly / 12 }
-        : undefined,
-      socialRentMonthlyLand: household.tenure.socialRent?.socialRentMonthlyLand
-        ? { socialRentMonthlyLand: household.tenure.socialRent.socialRentMonthlyLand }
-        : undefined,
-      mortgageFairholdLandPurchase: household.tenure.fairholdLandPurchase
-        ?.discountedLandMortgage
-        ? {
-            monthlyPayment:
-              household.tenure.fairholdLandPurchase.discountedLandMortgage
-                .monthlyPayment || 0,
-          }
-        : undefined,
-      fairholdLandRent: household.tenure.fairholdLandRent?.discountedLandRentYearly
-        ? {
-            discountedLandRent:
-              household.tenure.fairholdLandRent.discountedLandRentYearly / 12 || 0,
-          }
-        : undefined,
-      mortgageHouse: household.tenure.marketPurchase?.houseMortgage
-        ? {
-            monthlyPayment:
-              household.tenure.marketPurchase.houseMortgage.monthlyPayment || 0,
-          }
-        : undefined,
-      mortgageDepreciatedHouse: household.tenure.fairholdLandPurchase?.depreciatedHouseMortgage
-        ? {
-            monthlyPayment:
-              household.tenure.fairholdLandPurchase.depreciatedHouseMortgage
-                .monthlyPayment || 0,
-          }
-        : undefined,
-      averageRentHouse: household.tenure.marketRent?.averageRentHouseYearly
-        ? {
-          averageRentHouse:
-            household.tenure.marketRent?.averageRentHouseYearly / 12|| 0,
-        }
-      : undefined,
-      socialRentMonthlyHouse: household.tenure.socialRent?.socialRentMonthlyHouse
-        ? {
-          socialRentMonthlyHouse:
-          household.tenure.socialRent?.socialRentMonthlyHouse || 0,
-        }
-      : undefined,
-    };
-  };
-
-  const formatData = (household: TenureComparisonWrapperProps) => {
-    const toNumber = (value: number | { [key: string]: number } | undefined) =>
-      typeof value === 'number' ? value : value ? Object.values(value)[0] : 0;
-
+  const formatData = (household: Household) => {
     return [
       {
         category: "Monthly Costs Land",
-        marketPurchase: household.mortgageLand?.monthlyPayment || 0,
-        marketRent: toNumber(household.averageRentLand) || 0,
-        socialRent: toNumber(household.socialRentMonthlyLand) || 0,
-        fairholdLandPurchase:
-          household.mortgageFairholdLandPurchase?.monthlyPayment || 0,
-        fairholdLandRent: toNumber(household.fairholdLandRent) || 0,
+        marketPurchase: household.tenure.marketPurchase?.landMortgage?.monthlyPayment || 0,
+        marketRent: household.tenure.marketRent?.averageRentLandYearly ? household.tenure.marketRent.averageRentLandYearly / 12 : 0,
+        socialRent: household.tenure.socialRent?.socialRentMonthlyLand || 0,
+        fairholdLandPurchase: household.tenure.fairholdLandPurchase?.discountedLandMortgage?.monthlyPayment || 0,
+        fairholdLandRent: household.tenure.fairholdLandRent?.discountedLandRentYearly ? household.tenure.fairholdLandRent.discountedLandRentYearly / 12 : 0
       },
       {
         category: "Monthly Costs House",
-        marketPurchase: household.mortgageHouse?.monthlyPayment || 0,
-        marketRent: toNumber(household.averageRentHouse) || 0,
-        socialRent: toNumber(household.socialRentMonthlyHouse) || 0,
-        fairholdLandPurchase:
-          household.mortgageDepreciatedHouse?.monthlyPayment || 0,
-        fairholdLandRent:
-          household.mortgageDepreciatedHouse?.monthlyPayment || 0,
+        marketPurchase: household.tenure.marketPurchase?.houseMortgage?.monthlyPayment || 0,
+        marketRent: household.tenure.marketRent?.averageRentHouseYearly ? household.tenure.marketRent.averageRentHouseYearly / 12 : 0,
+        socialRent: household.tenure.socialRent?.socialRentMonthlyHouse || 0,
+        fairholdLandPurchase: household.tenure.fairholdLandPurchase?.depreciatedHouseMortgage?.monthlyPayment || 0,
+        fairholdLandRent: household.tenure.fairholdLandPurchase?.depreciatedHouseMortgage?.monthlyPayment || 0,
       },
     ];
   };
 
-  const formattedProps = transformHouseholdToProps(household);
-  const formattedData = formatData(formattedProps);
+  const formattedData = formatData(household);
 
   return (
     <div>
