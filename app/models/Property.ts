0d5c2c7c8c14e2253a4e5fce14dc25a5134dc923
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-07-28 11:34:50 +0100

    feat: Add ConstructorParams to avoid type repetition

--- app/models/Property.ts
+++ app/models/Property.ts
@@ -1,7 +1,23 @@
 
 import * as math from "mathjs";
 
+/**
+ * Number of decimal places to use when rounding numerical values
+ */
+const PRECISION = 2
 const DEPRECIATION_FACTOR = -32938;
+
+type PropertyParams = Pick<
+  Property,
+  | "postcode"
+  | "houseType"
+  | "numberOfBedrooms"
+  | "age"
+  | "size"
+  | "newBuildPricePerMetre"
+  | "averageMarketPrice"
+  | "itl3"
+>;
 
 // TODO: Reuse HouseType enum
 type HouseType = "D" | "S" | "T" | "F";
@@ -39,56 +55,37 @@ export class Property {
    */
   landToTotalRatio: number;
 
-  constructor({
-    postcode,
-    houseType,
-    numberOfBedrooms,
-    age,
-    size,
-    newBuildPricePerMetre,
-    averageMarketPrice,
-    itl3,
-  }: {
-    postcode: any;
-    houseType: HouseType;
-    numberOfBedrooms: number;
-    age: number;
-    size: number;
-    newBuildPricePerMetre: number;
-    averageMarketPrice: number;
-    itl3: string;
-  }) {
-    this.postcode = postcode;
-    this.houseType = houseType;
-    this.numberOfBedrooms = numberOfBedrooms;
-    this.age = age;
-    this.size = size;
-    this.newBuildPricePerMetre = newBuildPricePerMetre;
-    this.averageMarketPrice = averageMarketPrice;
-    this.itl3 = itl3;
+  constructor(params: PropertyParams) {
+    this.postcode = params.postcode;
+    this.houseType = params.houseType;
+    this.numberOfBedrooms = params.numberOfBedrooms;
+    this.age = params.age;
+    this.size = params.size;
+    this.newBuildPricePerMetre = params.newBuildPricePerMetre;
+    this.averageMarketPrice = params.averageMarketPrice;
+    this.itl3 = params.itl3;
+
+    // Computed properties, order is significant
     this.newBuildPrice = this.calculateNewBuildPrice();
     this.depreciatedBuildPrice = this.calculateDepreciatedBuildPrice();
     this.bedWeightedAveragePrice = this.calculateBedWeightedAveragePrice();
     this.landPrice = this.averageMarketPrice - this.newBuildPrice;
-    this.landToTotalRatio = this.landPrice / this.bedWeightedAveragePrice; 
+    this.landToTotalRatio = this.landPrice / this.bedWeightedAveragePrice;
   }
 
-  private calculateNewBuildPrice(precisionRounding: number = 2) {
+  private calculateNewBuildPrice() {
     let newBuildPrice = this.newBuildPricePerMetre * this.size;
-    newBuildPrice = parseFloat(newBuildPrice.toFixed(precisionRounding));
+    newBuildPrice = parseFloat(newBuildPrice.toFixed(PRECISION));
 
     return newBuildPrice;
   }
 
-  private calculateDepreciatedBuildPrice(
-    precisionRounding: number = 2
-  ) {
-
+  private calculateDepreciatedBuildPrice() {
     let depreciatedBuildPrice =
       this.newBuildPrice + DEPRECIATION_FACTOR * math.log(this.age);
     depreciatedBuildPrice = parseFloat(
-      depreciatedBuildPrice.toFixed(precisionRounding)
-    ); 
+      depreciatedBuildPrice.toFixed(PRECISION)
+    );
 
     return depreciatedBuildPrice;
   }
@@ -96,7 +93,6 @@ export class Property {
   private calculateBedWeightedAveragePrice(
     beds: number[] = [0, 1, 2, 3, 4, 5, 6],
     bedWeights: number[] = [0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4],
-    precisionRounding: number = 2
   ) {
     let bedWeight;
 
@@ -109,7 +105,7 @@ export class Property {
     }
 
     bedWeight = parseFloat(
-      (bedWeight * this.averageMarketPrice).toFixed(precisionRounding)
+      (bedWeight * this.averageMarketPrice).toFixed(PRECISION)
     );
 
     return bedWeight;
