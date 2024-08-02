3459c51e1adcc18f1d3a3bfd18a5c38da78a87e4
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-08-02 16:20:21 +0100

    fix: Better type narrowing in calculationSchema

--- app/schemas/calculationSchema.ts
+++ app/schemas/calculationSchema.ts
@@ -2,6 +2,9 @@ import { z } from "zod";
 import { parse as parsePostcode, fix as fixPostcode } from "postcode";
 import { HOUSE_TYPES } from "../models/Property";
 
+// Type not exported by postcode lib directly
+type ValidPostcode = Extract<ReturnType<typeof parsePostcode>, { valid: true }>;
+
 const HouseTypeEnum = z.enum(HOUSE_TYPES);
 
 /**
@@ -13,12 +16,12 @@ export const calculationSchema = z.object({
     .min(1, "housePostcode is required")
     .refine(fixPostcode, "Invalid postcode")
     .transform(parsePostcode)
-    .refine((postcode) => postcode.valid, { message: "Invalid postcode" }),
+    .refine((postcode): postcode is ValidPostcode => postcode.valid),
   houseSize: z.coerce.number().positive("houseSize must be a positive integer"),
   houseAge: z.coerce.number().positive("houseAge must be a positive integer"),
   houseBedrooms: z.coerce
     .number()
-    .positive("houseBedroomsmust be a positive integer"),
+    .positive("houseBedrooms must be a positive integer"),
   houseType: HouseTypeEnum.refine(
     (value) => HouseTypeEnum.options.includes(value),
     {
