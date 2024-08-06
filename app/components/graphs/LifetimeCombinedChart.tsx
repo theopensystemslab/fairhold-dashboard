932ba2ea449f710f4d5a57b22453b32ccb9fac70
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-08-06 09:58:13 +0100

    chore: Setup Prettier (#43)
    * chore: Setup prettier
    
    * chore: Run prettier on all current files
    
    * chore: Add script to package.json

--- app/components/graphs/LifetimeCombinedChart.tsx
+++ app/components/graphs/LifetimeCombinedChart.tsx
@@ -1,144 +1,156 @@
 "use client";
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 import {
-    Chart,
-    ChartData,
-    ChartDataset,
-    ChartOptions,
-    ChartTypeRegistry,
-    PointElement,
-    BarElement,
-    BarController,
-    LineController, 
-    LineElement,
-    CategoryScale,
-    LinearScale,
-    Tooltip,
-    Legend
-} from 'chart.js';
+  Chart,
+  ChartData,
+  ChartDataset,
+  ChartOptions,
+  ChartTypeRegistry,
+  PointElement,
+  BarElement,
+  BarController,
+  LineController,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
-Chart.register(PointElement, BarElement, BarController, LineController, LineElement, CategoryScale, LinearScale, Tooltip, Legend);
+Chart.register(
+  PointElement,
+  BarElement,
+  BarController,
+  LineController,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend
+);
 
 type DataInput = {
-    year: string;
-    landCost: number;
-    houseCost: number;
-    maintenanceCost?: number;
-    billsCost?: number;
-    incomeThreshold: number;
-}
+  year: string;
+  landCost: number;
+  houseCost: number;
+  maintenanceCost?: number;
+  billsCost?: number;
+  incomeThreshold: number;
+};
 
 interface ColorScheme {
-    land: string;
-    house: string;
-    maintenance: string;
-    bills: string;
-    incomeThreshold: string;
+  land: string;
+  house: string;
+  maintenance: string;
+  bills: string;
+  incomeThreshold: string;
 }
 
 interface LifetimeChartProps {
-    data: DataInput[];
-    colorScheme: ColorScheme;
+  data: DataInput[];
+  colorScheme: ColorScheme;
 }
 
 // Implementation of combination bar and line chart
-const LifetimeCombinedChart: React.FC<LifetimeChartProps> = ({ data, colorScheme }) => {
+const LifetimeCombinedChart: React.FC<LifetimeChartProps> = ({
+  data,
+  colorScheme,
+}) => {
+  const ref = useRef<HTMLCanvasElement>(null);
+  const chartRef = useRef<Chart | null>(null);
 
-    const ref = useRef<HTMLCanvasElement>(null);
-    const chartRef = useRef<Chart | null>(null);
+  useEffect(() => {
+    if (!ref.current) return;
 
-    useEffect(() => {
-        if (!ref.current) return;
+    // Transform the data into a format suitable for Chart.js
+    const years = data.map((item) => item.year);
+    const landData = data.map((item) => item.landCost);
+    const houseData = data.map((item) => item.houseCost);
+    const maintenanceData = data.map((item) => item.maintenanceCost ?? null);
+    const billsData = data.map((item) => item.billsCost ?? null);
+    const affordabilityThreshold = data.map((item) => item.incomeThreshold);
 
-        // Transform the data into a format suitable for Chart.js
-        const years = data.map(item => item.year);
-        const landData = data.map(item => item.landCost);
-        const houseData = data.map(item => item.houseCost);
-        const maintenanceData = data.map(item => item.maintenanceCost ?? null);
-        const billsData = data.map(item => item.billsCost ?? null);
-        const affordabilityThreshold = data.map(item => item.incomeThreshold);
+    const datasets: ChartDataset<"bar" | "line", number[]>[] = [
+      {
+        type: "bar",
+        label: "Land",
+        data: landData,
+        backgroundColor: colorScheme.land,
+        order: 1,
+      },
+      {
+        type: "bar",
+        label: "House",
+        data: houseData,
+        backgroundColor: colorScheme.house,
+        order: 2,
+      },
+      {
+        type: "line",
+        label: "Affordability threshold",
+        data: affordabilityThreshold,
+        borderColor: colorScheme.incomeThreshold,
+        backgroundColor: "transparent",
+        order: 0,
+      },
+    ];
+    // Only add maintenance data if it exists
+    if (maintenanceData.some((value) => value !== null)) {
+      datasets.push({
+        type: "bar",
+        label: "Maintenance",
+        data: maintenanceData.map((value) => value ?? 0),
+        backgroundColor: colorScheme.maintenance,
+        order: 3,
+      });
+    }
+    // Only add bills data if it exists
+    if (billsData.some((value) => value !== null)) {
+      datasets.push({
+        type: "bar",
+        label: "Bills",
+        data: billsData.map((value) => value ?? 0),
+        backgroundColor: colorScheme.bills,
+        order: 2,
+      });
+    }
 
-        const datasets: ChartDataset<'bar' | 'line', number[]>[] = [
-          {
-              type: 'bar',
-              label: 'Land',
-              data: landData,
-              backgroundColor: colorScheme.land,
-              order: 1,
-          },
-          {
-              type: 'bar',
-              label: 'House',
-              data: houseData,
-              backgroundColor: colorScheme.house,
-              order: 2,
-          },
-          {
-              type: 'line',
-              label: 'Affordability threshold',
-              data: affordabilityThreshold,
-              borderColor: colorScheme.incomeThreshold,
-              backgroundColor: 'transparent',
-              order: 0,
-          },
-      ];
-      // Only add maintenance data if it exists
-      if (maintenanceData.some(value => value !== null)) {
-          datasets.push({
-              type: 'bar',
-              label: 'Maintenance',
-              data: maintenanceData.map(value => value ?? 0),
-              backgroundColor: colorScheme.maintenance,
-              order: 3,
-          });
-      }
-      // Only add bills data if it exists
-      if (billsData.some(value => value !== null)) {
-          datasets.push({
-              type: 'bar',
-              label: 'Bills',
-              data: billsData.map(value => value ?? 0),
-              backgroundColor: colorScheme.bills,
-              order: 2,
-          });
-      }
+    const chartData: ChartData<"bar" | "line", number[], string> = {
+      labels: years,
+      datasets: datasets,
+    };
 
-        const chartData: ChartData<'bar' | 'line', number[], string> = {
-            labels: years,
-            datasets: datasets,
-        };
+    // Clear the canvas before re-drawing
+    if (chartRef.current) {
+      chartRef.current.destroy();
+    }
 
-        // Clear the canvas before re-drawing
-        if (chartRef.current) {
-            chartRef.current.destroy();
-        }
-
-        const newChartInstance = new Chart(ref.current, {
-            type: 'bar',
-            data: chartData,
-            options: {
-              responsive: true,
-              scales: {
-                x: {
-                  stacked: true,
-                  title: {
-                    display: true,
-                    text: 'Year',
-                  },
-                },
-                y: {
-                  stacked: true,
-                  title: {
-                    display: true,
-                    text: 'Yearly payments',
-                  },
-                },
-              },
+    const newChartInstance = new Chart(ref.current, {
+      type: "bar",
+      data: chartData,
+      options: {
+        responsive: true,
+        scales: {
+          x: {
+            stacked: true,
+            title: {
+              display: true,
+              text: "Year",
             },
-          });
+          },
+          y: {
+            stacked: true,
+            title: {
+              display: true,
+              text: "Yearly payments",
+            },
+          },
+        },
+      },
+    });
 
-          chartRef.current = newChartInstance;
+    chartRef.current = newChartInstance;
 
     // Cleanup function to destroy the chart instance when the component unmounts
     return () => {
@@ -146,7 +158,7 @@ const LifetimeCombinedChart: React.FC<LifetimeChartProps> = ({ data, colorScheme
         chartRef.current.destroy();
       }
     };
-    }, [data, colorScheme]);
+  }, [data, colorScheme]);
 
   return <canvas id={`chart-${colorScheme}`} ref={ref}></canvas>;
 };
--- app/components/graphs/LifetimeFairholdLandRentWrapper.tsx
+++ app/components/graphs/LifetimeFairholdLandRentWrapper.tsx
@@ -1,47 +1,48 @@
 "use client";
-import React from 'react';
-import { Household } from '@/app/models/Household';
-import LifetimeCombinedChart from './LifetimeCombinedChart';
+import React from "react";
+import { Household } from "@/app/models/Household";
+import LifetimeCombinedChart from "./LifetimeCombinedChart";
 interface Props {
-    household: Household;
+  household: Household;
 }
 
-const LifetimeFairholdLandRentWrapper: React.FC<Props> = ({ household  }) => {
-
+const LifetimeFairholdLandRentWrapper: React.FC<Props> = ({ household }) => {
   // Create color scheme for different versions of the graph
   const colorScheme = {
-    land: '#6c2091',
-    house: '#b275d1',
-    maintenance: '#595959',
-    bills: '#adadad',
-    incomeThreshold: '#6c9e6e'
-    }
+    land: "#6c2091",
+    house: "#b275d1",
+    maintenance: "#595959",
+    bills: "#adadad",
+    incomeThreshold: "#6c9e6e",
+  };
 
-    // Process and format the data for the chart
-    const chartData = (household.tenure.fairholdLandRent?.lifetime || []).map((item, index) => {
-      // Access and store household.lifetime array 
+  // Process and format the data for the chart
+  const chartData =
+    (household.tenure.fairholdLandRent?.lifetime || []).map((item, index) => {
+      // Access and store household.lifetime array
       // TODO remove defensive code after types tightened upstream
-      const incomeThreshold = household.lifetime && household.lifetime[index]
-        ? household.lifetime[index].affordabilityThresholdIncome
-        : 0;
-      
-        // Access and store all other variables
-      return {
-      year: index.toString(),
-      landCost: item.fairholdRentLand,
-      houseCost: item.houseMortgagePaymentYearly,
-      maintenanceCost: item.maintenanceCost,
-      // billsCost: item.gasBillYearly,
-      incomeThreshold: incomeThreshold,
-    };
-  }) ?? [];
+      const incomeThreshold =
+        household.lifetime && household.lifetime[index]
+          ? household.lifetime[index].affordabilityThresholdIncome
+          : 0;
 
-    return (
-      <div>
-        <h2>Lifetime Costs Chart</h2>
-        <LifetimeCombinedChart data={chartData} colorScheme={colorScheme} />
-      </div>
-    );
+      // Access and store all other variables
+      return {
+        year: index.toString(),
+        landCost: item.fairholdRentLand,
+        houseCost: item.houseMortgagePaymentYearly,
+        maintenanceCost: item.maintenanceCost,
+        // billsCost: item.gasBillYearly,
+        incomeThreshold: incomeThreshold,
+      };
+    }) ?? [];
+
+  return (
+    <div>
+      <h2>Lifetime Costs Chart</h2>
+      <LifetimeCombinedChart data={chartData} colorScheme={colorScheme} />
+    </div>
+  );
 };
-  
+
 export default LifetimeFairholdLandRentWrapper;
