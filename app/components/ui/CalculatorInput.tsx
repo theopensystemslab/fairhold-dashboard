5a2c927573364db6a45830049b7a8c32c82c8627
Gabriele Granello <gabriele@opensystemslab.io>
2024-08-07 12:03:16 +0200

    wip: react form hook

--- app/components/ui/CalculatorInput.tsx
+++ app/components/ui/CalculatorInput.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { trueDependencies } from "mathjs";
 import React, { useState } from "react";
+import { useForm, UseFormRegister } from "react-hook-form";
 import calculateFairhold from "@/app/models/testClasses";
 import { Household } from "@/app/models/Household";
 import Dashboard from "./Dashboard";
 
 const CalculatorInput = () => {
+  const { register, handleSubmit } = useForm();
   // create different view states: one for form and one for graph dashboard
   const [view, setView] = useState("form");
   const [data, setData] = useState<Household | null>(null);
@@ -16,34 +17,88 @@ const CalculatorInput = () => {
     Terrace: "T",
     Flat: "F",
   }; // variables associated to the house types
-  const [houseType, setHouseType] = useState(houseTypes.Detached); // variables associated to the house type
-  const [houseBedrooms, setHouseBedrooms] = useState(""); // variables associated to the number of bedrooms in the house
-  const [howSize, setHouseSize] = useState(""); // variables associated to the house size
-  const [houseAge, setHouseAge] = useState(""); // variables associated to the house age
+
+  // Define the props type
+  interface radioButtonProps {
+    id: string;
+    value: string;
+    label: string;
+    register: UseFormRegister<any>;
+  }
+
+  const RadioButtonsHousetype: React.FC<radioButtonProps> = ({
+    id,
+    value,
+    label,
+    register,
+  }) => {
+    return (
+      <label className="mx-2">
+        <input
+          className="accent-black"
+          type="radio"
+          id={id}
+          {...register("houseType")}
+          value={value}
+        />
+        {label}
+      </label>
+    );
+  };
+
+  interface inputEntryProps {
+    id: string;
+    placeholder: string;
+    register: UseFormRegister<any>;
+    type: string;
+  }
+  const InputEntryField: React.FC<inputEntryProps> = ({
+    id,
+    placeholder,
+    register,
+    type,
+  }) => {
+    return (
+      <input
+        className="mb-3 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+       rounded-md "
+        type={type}
+        placeholder={placeholder}
+        {...register(id)}
+        id={id}
+        key={id}
+      />
+    );
+  };
+  // const [houseType, setHouseType] = useState(houseTypes.Detached); // variables associated to the house type
+  // const [houseBedrooms, setHouseBedrooms] = useState(""); // variables associated to the number of bedrooms in the house
+  // const [howSize, setHouseSize] = useState(""); // variables associated to the house size
+  // const [houseAge, setHouseAge] = useState(""); // variables associated to the house age
 
   // fucntion that defines what happens after submitting the form
-  async function handleSubmit(e: any) {
-    e.preventDefault(); // pr event the default of the form
-    const formData = new FormData(e.currentTarget); // get the data in the form, e.g postcode, house size etc
-    const data = Object.fromEntries(formData.entries());
+  // handleSubmit(e: any) =>{
+  //   e.preventDefault(); // pr event the default of the form
+  //   const formData = new FormData(e.currentTarget); // get the data in the form, e.g postcode, house size etc
+  //   const data = Object.fromEntries(formData.entries());
 
-    //fetch the data: call the api and attach the form data
-    const response = await fetch("/api", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data), // pass the form data to the API
-    });
-    const jsonData = await response.json();
-    console.log("handleSubmit jsonData: ", jsonData);
-    const processedData = calculateFairhold(jsonData);
-    console.log("handleSubmit processedData: ", processedData);
+  //fetch the data: call the api and attach the form data
+  // const response = await fetch("/api", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(data), // pass the form data to the API
+  // });
+  // const jsonData = await response.json();
+  // console.log("handleSubmit jsonData: ", jsonData);
+  // const processedData = calculateFairhold(jsonData);
+  // console.log("handleSubmit processedData: ", processedData);
 
-    // saved processedData & switch to dashboard view
-    setData(processedData);
-    setView("dashboard");
-  }
+  // // saved processedData & switch to dashboard view
+  // setData(processedData);
+  // setView("dashboard");
+  // }
 
   return view === "form" ? (
     <div className="flex -centeitemsr justify-center text-black mt-5">
@@ -51,105 +106,54 @@ const CalculatorInput = () => {
         <div className="bg-black text-white h-48 flex items-center justify-center">
           <h1 className="text-6xl">Fairhold Calculator</h1>
         </div>
-        <form onSubmit={handleSubmit} className=" flex flex-col m-5">
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}
+          className=" flex flex-col m-5"
+        >
           <h2 className="mb-1 font-bold">House postcode</h2>
-          <input
-            className="mb-3 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-             rounded-md "
+          <InputEntryField
+            id="housePostcode"
             type="text"
-            placeholder="e.g. SE17 1PE"
-            value={housePostcode}
-            onChange={(e) => sethousePostcode(e.target.value)}
-            name="housePostcode"
+            placeholder="set the postcode, e.g. SE17 1PE"
+            register={register}
           />
+
           <h2 className="mb-1 font-bold">House size</h2>
-          <input
-            className="mb-3 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-             rounded-md "
+          <InputEntryField
             type="number"
             placeholder="Provide the house size in m square, e.g. 66"
-            value={howSize}
-            onChange={(e) => setHouseSize(e.target.value)}
-            name="houseSize"
+            id="howSize"
+            register={register}
           />
+
           <h2 className="mb-1 font-bold">House typology</h2>
           <div className="flex">
-            <label className="mx-2">
-              <input
-                className="accent-black"
-                type="radio"
-                id="Detached"
-                name="houseType"
-                value={houseTypes.Detached}
-                checked={houseType === houseTypes.Detached}
-                onChange={() => setHouseType(houseTypes.Detached)}
+            {Object.entries(houseTypes).map(([label, value]) => (
+              <RadioButtonsHousetype
+                key={label}
+                label={label}
+                id={label}
+                value={value}
+                register={register}
               />
-              Detached
-            </label>
-
-            <label className="mx-2">
-              <input
-                className="accent-black"
-                type="radio"
-                id="Semidetached"
-                name="houseType"
-                value={houseTypes.Semidetached}
-                checked={houseType === houseTypes.Semidetached}
-                onChange={() => setHouseType(houseTypes.Semidetached)}
-              />
-              Semi-Detached
-            </label>
-
-            <label className="mx-2">
-              <input
-                className="accent-black"
-                type="radio"
-                id="Terrace"
-                name="houseType"
-                value={houseTypes.Terrace}
-                checked={houseType === houseTypes.Terrace}
-                onChange={() => setHouseType(houseTypes.Terrace)}
-              />
-              Terrace
-            </label>
-
-            <label className="mx-2">
-              <input
-                className="accent-black"
-                type="radio"
-                id="Flat"
-                name="houseType"
-                value={houseTypes.Flat}
-                checked={houseType === houseTypes.Flat}
-                onChange={() => setHouseType(houseTypes.Flat)}
-              />
-              Flat
-            </label>
+            ))}
           </div>
 
           <h2 className="mb-1 font-bold">House age</h2>
-          <input
-            className="mb-3 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-             rounded-md "
+          <InputEntryField
             type="number"
             placeholder="Provide the house age in years. For a new build, insert age 1"
-            value={houseAge}
-            onChange={(e) => setHouseAge(e.target.value)}
-            name="houseAge"
+            id="houseAge"
+            register={register}
           />
           <h2 className="mb-1 font-bold">Number of bedrooms</h2>
-          <input
-            className="mb-3 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-             rounded-md "
+          <InputEntryField
             type="number"
             placeholder="Provide the number of bedrooms e.g. 2"
-            value={houseBedrooms}
-            onChange={(e) => setHouseBedrooms(e.target.value)}
-            name="houseBedrooms"
+            id="houseBedrooms"
+            register={register}
           />
           <button
             className="text-white bg-black w-1/3 rounded-xl"
