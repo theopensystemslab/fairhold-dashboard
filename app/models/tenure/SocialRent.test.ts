d0a36ea8606a1fdc2acc45a09df4c19e470413d1
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-08-07 09:47:17 +0100

    chore: Setup linting on project (#41)
    * chore: Setup linting rules
    
    * fix: Linting issues
    
    * fix: Outstanding linting fixes
    
    * chore: Tidy up eslint
    
    * chore: Run prettier after rebase
    
    * fix: Add back `socialRentTotalEarning`
    - Please see https://github.com/theopensystemslab/fairhold-dashboard/pull/41\#discussion_r1701743583

--- app/models/tenure/SocialRent.test.ts
+++ app/models/tenure/SocialRent.test.ts
@@ -1,31 +1,9 @@
-import { Property } from "../Property";
 import { SocialRent, SocialRentAdjustments } from "./SocialRent";
 
 let tenureSocialRent: SocialRent;
 
 beforeEach(() => {
-  let forecastParameters = {
-    maintenanceCostPercentage: 0.0125, // percentage maintenance cost
-    incomeGrowthPerYear: 0.04, // 4% income growth per year
-    constructionPriceGrowthPerYear: 0.025, // 2.5%
-    rentGrowthPerYear: 0.03, // 3%
-    propertyPriceGrowthPerYear: 0.05, // 5%
-    yearsForecast: 40, // 40 years
-    affordabilityThresholdIncomePercentage: 0.35, // percentage of imcome to afford rent or purchase
-  };
-
-  let property = new Property({
-    postcode: "WV8 1HG",
-    houseType: "T",
-    numberOfBedrooms: 2,
-    age: 10,
-    size: 88,
-    newBuildPricePerMetre: 2120,
-    averageMarketPrice: 218091.58,
-    itl3: "TLG24",
-  });
-
-  let socialRentAdjustments: SocialRentAdjustments = [
+  const socialRentAdjustments: SocialRentAdjustments = [
     {
       inflation: 3.3,
       total: 4.3,
