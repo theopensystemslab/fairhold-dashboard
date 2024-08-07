12284fd3c2139d3d7d151472150a4a81597f3d4d
Gabriele Granello <gabriele@opensystemslab.io>
2024-08-07 17:22:41 +0200

    Added form validation with zod and refactored the components

---
+++ app/components/ui/InputField.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { UseFormRegister } from "react-hook-form";
+
+interface InputFieldProps {
+  id: string;
+  placeholder: string;
+  type: string;
+  register: UseFormRegister<any>;
+  error?: string;
+}
+
+const InputField: React.FC<InputFieldProps> = ({
+  id,
+  placeholder,
+  register,
+  type,
+  error,
+}) => {
+  return (
+    <>
+      <input
+        className="mb-3 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+       rounded-md "
+        type={type}
+        placeholder={placeholder}
+        {...register(id)}
+        id={id}
+        key={id}
+      />
+      {error && <p className="text-red-500">{error}</p>}
+    </>
+  );
+};
+export default InputField;
