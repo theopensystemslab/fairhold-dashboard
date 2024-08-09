3571998e91607b8f8dbdb5a56e076c4a46f31e42
Gabriele Granello <gabriele@opensystemslab.io>
2024-08-09 16:06:30 +0200

    pricesPaid refactoring

---
+++ app/data/pricesPaidRepo.ts
@@ -0,0 +1,119 @@
+import prisma from "./db";
+
+const MINIMUM_NUMBER_OF_POSTCODES = 30;
+
+interface pricesPaidParams {
+  averagePrice: number;
+  numberOfTransactions: number;
+  granularityPostcode: string;
+}
+const getPricesPaidByPostcodeAndHouseType = async (
+  postcodeDistrict: string,
+  postcodeArea: string,
+  postcodeSector: string,
+  houseType: string
+): Promise<pricesPaidParams> => {
+  try {
+    // create the progressive queries
+    let pricesPaid; // declare the variable for prices paid
+    let numberOfTransactions; // declare the variable for numbers of transactions retrieved
+    let granularityPostcode; // declare the granularity of the postcode
+    let averagePrice;
+
+    const pricesPaidSector = await prisma.pricespaid.aggregate({
+      where: {
+        propertytype: {
+          equals: houseType,
+        },
+        postcode: {
+          startsWith: postcodeSector,
+        },
+      },
+      _count: {
+        id: true,
+      },
+      _avg: {
+        price: true,
+      },
+    });
+
+    const numberPerSector = pricesPaidSector._count.id;
+    const isMinMetBySector = numberPerSector >= MINIMUM_NUMBER_OF_POSTCODES;
+
+    if (!isMinMetBySector) {
+      const pricesPaidDistrict = await prisma.pricespaid.aggregate({
+        where: {
+          propertytype: {
+            equals: houseType,
+          },
+          postcode: {
+            startsWith: postcodeDistrict,
+          },
+        },
+        _count: {
+          id: true,
+        },
+        _avg: {
+          price: true,
+        },
+      });
+
+      const numberPerDistrict = pricesPaidDistrict._count.id;
+      const isMinMetByDistrict =
+        numberPerDistrict >= MINIMUM_NUMBER_OF_POSTCODES;
+
+      if (!isMinMetByDistrict) {
+        const pricesPaidArea = await prisma.pricespaid.aggregate({
+          where: {
+            propertytype: {
+              equals: houseType,
+            },
+            postcode: {
+              startsWith: postcodeArea,
+            },
+          },
+          _count: {
+            id: true,
+          },
+          _avg: {
+            price: true,
+          },
+        });
+        const numberPerArea = pricesPaidArea._count.id;
+        pricesPaid = pricesPaidArea; // if condition is met, the granularity is appropriate
+        numberOfTransactions = numberPerArea; // check the granularity
+        granularityPostcode = postcodeArea; // granularity of the postcode when performing the average price search
+        averagePrice = pricesPaidArea._avg.price;
+      } else {
+        pricesPaid = pricesPaidDistrict; // if condition is met, the granularity is appropriate
+        numberOfTransactions = numberPerDistrict; // check the granularity
+        granularityPostcode = postcodeDistrict; // granularity of the postcode
+        averagePrice = pricesPaidDistrict._avg.price;
+      }
+    } else {
+      pricesPaid = pricesPaidSector; // if condition is met, the granularity is appropriate
+      numberOfTransactions = numberPerSector; // check the granularity
+      granularityPostcode = postcodeSector; // granularity of the postcode
+      averagePrice = pricesPaidSector._avg.price;
+    }
+
+    if (averagePrice === null) {
+      throw new Error("Unable to calculate average price");
+    }
+
+    // Cast to string as 'not: null' clause in Prisma query does not type narrow
+    return {
+      averagePrice,
+      numberOfTransactions,
+      granularityPostcode,
+    } as pricesPaidParams;
+  } catch (error) {
+    throw new Error(
+      `Data error: Unable to get pricesPaid for postcode district ${postcodeDistrict} and houseType ${houseType}`
+    );
+  }
+};
+
+export const pricesPaidRepo = {
+  getPricesPaidByPostcodeAndHouseType,
+};
--- app/services/calculationService.ts
+++ app/services/calculationService.ts
@@ -1,13 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { itlService } from "./itlService";
 import { gdhiService } from "./gdhiService";
+import { pricesPaidService } from "./pricesPaidService";
 import { Calculation } from "../schemas/calculationSchema";
 
 const prisma = new PrismaClient();
 
-export const getHouseholdData = async (
-  input: Calculation
-) => {
+export const getHouseholdData = async (input: Calculation) => {
   try {
     // data are going to be queried at different levels of granularity based on the postcode
     const postcode = input.housePostcode;
@@ -15,93 +14,15 @@ export const getHouseholdData = async (
     const postcodeDistrict = postcode.district; // extract only characters for the district, SE17
     const postcodeSector = postcode.sector; // extract only the characters for the sector, SE17 1
 
-    // create the progressive queries
-    const minimumNumberPostcodes = 30; // minimum number of entries to create the average
-    let pricesPaid; // declare the variable for prices paid
-    let numberOfTransactions; // declare the variable for numbers of transactions retrieved
-    let granularityPostcode; // declare the granularity of the postcode
-    let averagePrice;
-
-    const pricesPaidSector = await prisma.pricesPaid.aggregate({
-      where: {
-        propertyType: {
-          equals: input.houseType,
-        },
-        postcode: {
-          startsWith: postcodeSector,
-        },
-      },
-      _count: {
-        id: true,
-      },
-      _avg: {
-        price: true,
-      },
-    });
-
-    const numberPerSector = pricesPaidSector._count.id;
-    const isMinMetBySector = numberPerSector >= minimumNumberPostcodes;
-
-    if (!isMinMetBySector) {
-      const pricesPaidDistrict = await prisma.pricesPaid.aggregate({
-        where: {
-          propertyType: {
-            equals: input.houseType,
-          },
-          postcode: {
-            startsWith: postcodeDistrict,
-          },
-        },
-        _count: {
-          id: true,
-        },
-        _avg: {
-          price: true,
-        },
-      });
-
-      const numberPerDistrict = pricesPaidDistrict._count.id;
-      const isMinMetByDistrict = numberPerDistrict >= minimumNumberPostcodes;
-
-      if (!isMinMetByDistrict) {
-        const pricesPaidArea = await prisma.pricesPaid.aggregate({
-          where: {
-            propertyType: {
-              equals: input.houseType,
-            },
-            postcode: {
-              startsWith: postcodeArea,
-            },
-          },
-          _count: {
-            id: true,
-          },
-          _avg: {
-            price: true,
-          },
-        });
-        const numberPerArea = pricesPaidArea._count.id;
-
-        pricesPaid = pricesPaidArea; // if condition is met, the granularity is appropriate
-        numberOfTransactions = numberPerArea; // check the granularity
-        granularityPostcode = postcodeArea; // granularity of the postcode when performing the average price search
-        averagePrice = pricesPaidArea._avg.price;
-      } else {
-        pricesPaid = pricesPaidDistrict; // if condition is met, the granularity is appropriate
-        numberOfTransactions = numberPerDistrict; // check the granularity
-        granularityPostcode = postcodeDistrict; // granularity of the postcode
-        averagePrice = pricesPaidDistrict._avg.price;
-      }
-    } else {
-      pricesPaid = pricesPaidSector; // if condition is met, the granularity is appropriate
-      numberOfTransactions = numberPerSector; // check the granularity
-      granularityPostcode = postcodeSector; // granularity of the postcode
-      averagePrice = pricesPaidSector._avg.price;
-    }
-
-    if (averagePrice === null) {
-      throw new Error("Unable to calculate average price");
-    }
+    const itl3 = await itlService.getByPostcodeDistrict(postcodeDistrict);
+    const gdhi = await gdhiService.getByITL3(itl3);
+    const { averagePrice, numberOfTransactions, granularityPostcode } =
+      await pricesPaidService.getPricesPaidByPostcodeAndHouseType(
+        postcodeDistrict,
+        postcodeArea,
+        postcodeSector,
+        input.houseType
+      );
 
     const { priceMid: buildPrice } = await prisma.buildPrices.findFirstOrThrow({
       where: {
@@ -111,9 +32,6 @@ export const getHouseholdData = async (
     });
     // TODO: Make columns non-nullable
     if (!buildPrice) throw Error("Missing buildPrice");
-
-    const itl3 = await itlService.getByPostcodeDistrict(postcodeDistrict);
-    const gdhi = await gdhiService.getByITL3(itl3);
 
     const {
       _avg: { monthlyMeanRent: averageRentMonthly },
@@ -187,7 +105,7 @@ export const getHouseholdData = async (
       granularityPostcode,
       pricesPaid,
       gasBillYearly,
-    }
+    };
   } catch (err) {
     throw Error(
       `Service error: Unable to generate household. Message: ${(err as Error).message}`
