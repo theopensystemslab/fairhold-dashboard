946f6690931d2a7e003a8ad2a820af2bcd8346cb
zzhhaa <zzhhaa@protonmail.com>
2024-08-16 15:45:39 +0100

    chore: passed ForecastParameters to tenure tests

--- app/models/tenure/FairholdLandPurchase.test.ts
+++ app/models/tenure/FairholdLandPurchase.test.ts
@@ -1,22 +1,26 @@
 import { Fairhold } from "../Fairhold";
-import { DEFAULT_FORECAST_PARAMETERS } from "../ForecastParameters";
+import { DEFAULT_FORECAST_PARAMETERS, ForecastParameters } from "../ForecastParameters";
 import { FairholdLandPurchase } from "./FairholdLandPurchase";
 
 let tenureFairholdLandPurchase: FairholdLandPurchase;
 
 beforeEach(() => {
+  const forecastParameters: ForecastParameters = {
+    ...DEFAULT_FORECAST_PARAMETERS,
+  };
+
   tenureFairholdLandPurchase = new FairholdLandPurchase({
     //incomeYearly: 45816,
     //averagePrice: 218091.58,
     newBuildPrice: 186560,
     depreciatedBuildPrice: 110717.45,
     //landPrice: 31531.579,
-    ...DEFAULT_FORECAST_PARAMETERS,
     affordability: 0.2,
     fairhold: new Fairhold({
       affordability: 0.2,
       landPriceOrRent: 31531.579,
     }),
+    forecastParameters: forecastParameters,
   });
 });
 
