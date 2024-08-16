28219512e3db4f94ef5cef715603a3dc217ad60a
zzhhaa <zzhhaa@protonmail.com>
2024-08-16 16:31:22 +0100

    feat: create gasBillRepo layer

---
+++ app/data/gasBillRepo.ts
@@ -0,0 +1,16 @@
+import prisma from "./db";
+
+const getGasBillByITL3 = async (itl: string): Promise<number> => {
+    try {
+        const { bill } = await prisma.gasBills.findFirstOrThrow({
+            where: {
+                itl: { startsWith: itl.substring(0, 3) },
+            },
+            select: { bill: true },
+        });
+
+        return bill as number;
+    } catch (error) {
+        throw Error(`Data error: Unable to find gas_bills_2020 for itl3 ${itl}`);
+    }
+};
