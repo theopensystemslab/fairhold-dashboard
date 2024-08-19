7e6af72e6cb7a6076c898db47fa9ee9c56752972
Gabriele Granello <gabriele@opensystemslab.io>
2024-08-19 15:31:04 +0200

    fixed wrong variable being passed

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,16 +2,14 @@ import { NextResponse } from "next/server";
 import { Calculation, calculationSchema } from "../schemas/calculationSchema";
 import * as calculationService from "../services/calculationService";
 
-
 export async function POST(req: Request) {
   try {
     // Parse and validate user input
     const data = await req.json();
     //const input: Calculation = calculationSchema.parse(data);
 
-    const householdData = await calculationService.getHouseholdData(input);
+    const householdData = await calculationService.getHouseholdData(data);
     return NextResponse.json(householdData);
-
   } catch (err) {
     console.log("ERROR: API - ", (err as Error).message);
     const response = { error: (err as Error).message };
