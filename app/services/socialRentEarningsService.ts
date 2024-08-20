6521d67635a6d071ccc43cd3f89455e44fde1291
zzhhaa <zzhhaa@protonmail.com>
2024-08-20 13:35:39 +0100

    fix: renamed socialRentEarningsService

--- app/services/socialRentEarningsService.ts
+++ app/services/socialRentEarningsService.ts
@@ -1,9 +1,9 @@
 import { socialRentEarningsRepo } from "../data/socialRentEarningsRepo";
 
 const getByITL3 = async (itl3: string) => {
-  return await socialRentEarningsRepo.getSocialRentEarningsByITL3(itl3);
+  return await socialRentEarningsRepo.getSocialRentEarningsByITL3(itl3)
 };
 
 export const socialRentEarningsService = {
   getByITL3,
-};
+}
