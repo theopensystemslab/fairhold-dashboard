850c0dd91300f7852ad00a736d196e4f0f07d4dc
zzhhaa <zzhhaa@protonmail.com>
2024-08-20 13:37:36 +0100

    fix: corrected aggregate function

--- app/data/hpiRepo.ts
+++ app/data/hpiRepo.ts
@@ -2,14 +2,19 @@ import prisma from "./db";
 
 const getHPIByITL3 = async (itl3: string): Promise<number> => {
     try {
-        const { hpi2020 } = await prisma.hPI.findFirstOrThrow({
+        const { 
+            _avg: { hpi2020: averageHpi },
+        } = await prisma.hPI.aggregate({
             where: {
-                itl3: { equals: itl3 },
+                itl3: {
+                endsWith: itl3,
+                },
             },
-            select: { hpi2020: true },
+            _avg: {
+                hpi2020: true,
+            },
         });
-
-        return hpi2020 as number;
+        return averageHpi as number;
     } catch (error) {
         throw Error(`Data error: Unable to find hpi2020 for itl3 ${itl3}`);
     }
--- app/data/socialRentEarningsRepo.ts
+++ app/data/socialRentEarningsRepo.ts
@@ -2,14 +2,25 @@ import prisma from "./db";
 
 const getSocialRentEarningsByITL3 = async (itl3: string): Promise<number> => {
     try {
-        const { earningsPerWeek } = await prisma.socialRent.findFirstOrThrow({
+        const result = await prisma.socialRent.aggregate({ 
             where: {
-                itl3: { equals: itl3 },
+              itl3: {
+                startsWith: itl3.substring(0, 3),
+              },
             },
-            select: { earningsPerWeek: true },
-        });
-
-        return earningsPerWeek as number;
+            _avg: {
+              earningsPerWeek: true,
+            },
+          });
+        
+        const monthlyMeanRent = result._avg.earningsPerWeek;
+        
+        if (monthlyMeanRent === null) {
+            throw new Error(`No data found for itl3 ${itl3}`);
+        }
+        
+        return monthlyMeanRent;
+        
     } catch (error) {
         throw Error(`Data error: Unable to find earningsPerWeek for itl3 ${itl3}`);
     }
@@ -17,5 +28,4 @@ const getSocialRentEarningsByITL3 = async (itl3: string): Promise<number> => {
 
 export const socialRentEarningsRepo = {
     getSocialRentEarningsByITL3,
-  };
-  
+};
