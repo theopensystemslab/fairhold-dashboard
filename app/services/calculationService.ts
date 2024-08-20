7344f85a1d7c9a8f8507801875335c6c527c83e5
zzhhaa <zzhhaa@protonmail.com>
2024-08-20 13:38:23 +0100

    feat: added all layers to calculationService

--- app/services/calculationService.ts
+++ app/services/calculationService.ts
@@ -1,8 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import { itlService } from "./itlService";
-import { gdhiService } from "./gdhiService";
-import { pricesPaidService } from "./pricesPaidService";
 import { buildPriceService } from "./buildPriceService";
+import { gasBillService } from "./gasBillService";
+import { gdhiService } from "./gdhiService";
+import { hpiService } from "./hpiService";
+import { itlService } from "./itlService";
+import { pricesPaidService } from "./pricesPaidService";
+import { socialRentAdjustmentsService } from "./socialRentAdjustmentsService";
+import { socialRentEarningsService } from "./socialRentEarningsService";
+import { rentService } from "./rentService"
 import { Calculation } from "../schemas/calculationSchema";
 
 const prisma = new PrismaClient();
@@ -17,6 +22,10 @@ export const getHouseholdData = async (input: Calculation) => {
 
     const itl3 = await itlService.getByPostcodeDistrict(postcodeDistrict);
     const gdhi = await gdhiService.getByITL3(itl3);
+    const gasBillYearly = await gasBillService.getByITL3(itl3);
+    const hpi = await hpiService.getByITL3(itl3);
+    const buildPrice = await buildPriceService.getBuildPriceByHouseType(input.houseType);
+
     const { averagePrice, numberOfTransactions, granularityPostcode } =
       await pricesPaidService.getPricesPaidByPostcodeAndHouseType(
         postcodeDistrict,
@@ -24,63 +33,11 @@ export const getHouseholdData = async (input: Calculation) => {
         postcodeSector,
         input.houseType
       );
-    const buildPrice = await buildPriceService.getBuildPriceByHouseType(
-      input.houseType
-    );
 
-    const {
-      _avg: { monthlyMeanRent: averageRentMonthly },
-    } = await prisma.rent.aggregate({
-      where: { itl3 },
-      _avg: {
-        monthlyMeanRent: true,
-      },
-    });
-    if (!averageRentMonthly) throw Error("Missing averageRentMonthly");
+    const averageRentMonthly = await rentService.getByITL3(itl3); 
 
-    const socialRentAdjustments = await prisma.socialRentAdjustments.findMany();
-    const itl3Prefix = itl3.substring(0, 4);
-
-    const {
-      _avg: { earningsPerWeek: socialRentAveEarning },
-    } = await prisma.socialRent.aggregate({
-      where: {
-        itl3: {
-          startsWith: itl3Prefix,
-        },
-      },
-      _avg: {
-        earningsPerWeek: true,
-      },
-    });
-
-    if (!socialRentAveEarning) throw Error("Missing socialRentAveEarning");
-
-    const {
-      _avg: { hpi2020: averageHpi },
-    } = await prisma.hPI.aggregate({
-      where: {
-        itl3: {
-          endsWith: itl3,
-        },
-      },
-      _avg: {
-        hpi2020: true,
-      },
-    });
-    if (!averageHpi) throw Error("Missing averageHpi");
-
-    const { bill: gasBillYearly } = await prisma.gasBills.findFirstOrThrow({
-      where: {
-        itl: {
-          startsWith: itl3.substring(0, 3),
-        },
-      },
-      select: {
-        bill: true,
-      },
-    });
-    if (!gasBillYearly) throw Error("Missing gasBillYearly");
+    const socialRentAdjustments = await socialRentAdjustmentsService.getAdjustments();
+    const socialRentAveEarning = await socialRentEarningsService.getByITL3(itl3)
 
     return {
       postcode: input.housePostcode,
@@ -91,7 +48,7 @@ export const getHouseholdData = async (input: Calculation) => {
       averagePrice: parseFloat(averagePrice.toFixed(2)),
       itl3,
       gdhi,
-      hpi: averageHpi,
+      hpi,
       buildPrice,
       averageRentMonthly,
       socialRentAdjustments,
