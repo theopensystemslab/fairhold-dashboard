2c6cd50a750912371d489a38d178d6378e0e4cb7
Gabriele Granello <gabriele@opensystemslab.io>
2024-08-23 15:51:54 +0200

    update calculation schema

--- app/components/ui/CalculatorInput.tsx
+++ app/components/ui/CalculatorInput.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { useForm, UseFormRegister } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import calculateFairhold from "@/app/models/testClasses";
--- app/schemas/calculationSchema.ts
+++ app/schemas/calculationSchema.ts
@@ -11,12 +11,19 @@ const HouseTypeEnum = z.enum(HOUSE_TYPES);
  * Describes the form the user will interact with in the frontend
  */
 export const calculationSchema = z.object({
-  housePostcode: z
-    .string()
-    .min(1, "housePostcode is required")
-    .refine(fixPostcode, "Invalid postcode")
-    .transform(parsePostcode)
-    .refine((postcode): postcode is ValidPostcode => postcode.valid),
+  housePostcode: z.union([
+    z
+      .string()
+      .min(1, "housePostcode is required")
+      .refine(fixPostcode, "Invalid postcode")
+      .transform(parsePostcode)
+      .refine(
+        (postcode): postcode is ValidPostcode => postcode.valid,
+        "Invalid postcode"
+      ),
+    z.custom<ValidPostcode>(),
+  ]),
+
   houseSize: z.coerce.number().positive("houseSize must be a positive integer"),
   houseAge: z.coerce.number().positive("houseAge must be a positive integer"),
   houseBedrooms: z.coerce
