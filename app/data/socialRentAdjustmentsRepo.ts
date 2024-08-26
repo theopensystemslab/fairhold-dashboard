dde813873f27d118cd7f49ebc4c643c923015f09
Gabriele Granello <gabriele@opensystemslab.io>
2024-08-26 11:12:35 +0200

    socialRentAdjustment types

--- app/data/socialRentAdjustmentsRepo.ts
+++ app/data/socialRentAdjustmentsRepo.ts
@@ -2,36 +2,51 @@ import prisma from "./db";
 
 export type socialRentAdjustmentTypes = {
   year: string;
-  inflation: string;
-  additional: string;
-  total: string;
+  inflation: number;
+  additional: number;
+  total: number;
 }[];
 
-const getSocialRentAdjustments = async (): Promise<socialRentAdjustmentTypes> => {
+const getSocialRentAdjustments =
+  async (): Promise<socialRentAdjustmentTypes> => {
     try {
-        const result = await prisma.socialRentAdjustments.findMany({
-            select: {
-                year: true,
-                inflation: true,
-                additional: true,
-                total: true,
-            }
-        });
+      const result = await prisma.socialRentAdjustments.findMany({
+        select: {
+          year: true,
+          inflation: true,
+          additional: true,
+          total: true,
+        },
+      });
 
-        const socialRentAdjustments: socialRentAdjustmentTypes = result.map(item => ({
-            year: item.year || '',
-            inflation: item.inflation?.toString() || '',
-            additional: item.additional?.toString() || '',
-            total: item.total?.toString() || '',
-        }));
+      const socialRentAdjustments: socialRentAdjustmentTypes = result.map(
+        (item) => {
+          if (
+            item.year === null ||
+            item.inflation === null ||
+            item.additional === null ||
+            item.total === null
+          ) {
+            throw new Error(
+              `Data error: Found null values in socialRentAdjustments`
+            );
+          }
+          return {
+            year: item.year,
+            inflation: item.inflation,
+            additional: item.additional,
+            total: item.total,
+          };
+        }
+      );
 
-        return socialRentAdjustments;
-        
+      // Return the array after validation
+      return socialRentAdjustments;
     } catch (error) {
-        throw new Error(`Data error: Unable to find socialRentAdjustments`);
+      throw new Error(`Data error: unable to find socialRentAdjustments`);
     }
-};
+  };
 
 export const socialRentAdjustmentsRepo = {
-    getSocialRentAdjustments,
+  getSocialRentAdjustments,
 };
