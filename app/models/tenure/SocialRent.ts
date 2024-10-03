9b5c2bbfeae9c81974888a0738e23fdaedb0ba8e
zzhhaa <zzhhaa@protonmail.com>
2024-10-03 11:48:28 +0100

    chore: add JSDoc to all properties and constructor

--- app/models/tenure/SocialRent.ts
+++ app/models/tenure/SocialRent.ts
@@ -2,27 +2,40 @@ import { WEEKS_PER_MONTH } from "../constants";
 import { BED_WEIGHTS_AND_CAPS, NATIONAL_AVERAGES } from "../constants";
 import { socialRentAdjustmentTypes } from "../../data/socialRentAdjustmentsRepo";
 
-/** Here are the variables (non-constant) needed to calculate social rent */
+/** These parameters are the necessary inputs for calculating the social rent formula. 
+ * They include `numberOfBedrooms` (for weighting the property size and checking against rent caps),
+ * `countyAverageEarnings1999` for calculating relative earnings, 
+ * inflation data for adjusting social rent upwards in `socialRentAdjustments`, 
+ * `hpi2000` with local property prices (TODO: fix to 1999),
+ * and `landToTotalRatio` for estimating a split between land and house within monthly housing costs.
+ */
 interface SocialRentParams {
+  /** Formula rent specifies different weightings and rent caps depending on the number of bedrooms in a dwelling. */
   numberOfBedrooms: number;
   countyAverageEarnings1999: number; 
-  /** An object that includes inflation and additional adjustments (by percentage) for uprating formula rent from 2000 values  */
   socialRentAdjustments: socialRentAdjustmentTypes;
-  averageMarketPrice2000: number;
+  hpi2000: number;
+  /** Using market values, calculates the ratio between land value and total value of a property. Used to estimate social rent land-house split. */
   landToTotalRatio: number;
 }
 
 export class SocialRent {
+  /** Relative earnings in 1999 are calculated using the county average as an input. */
   countyAverageEarnings1999: number;
+  /** An object that includes inflation and additional adjustments (by percentage) for uprating formula rent from 2000 values.  */
   socialRentAdjustments;
-  averageMarketPrice2000;
-  adjustedSocialRentMonthly: number; //adjusted social rent monthly
-  socialRentMonthlyLand: number; // social rent to pay the land
-  socialRentMonthlyHouse: number; // social rent monthly House
+  /** Relative property prices are calculated using 1999 values; our current HPI data goes back as far as 2000 so that is used for now. */ // TODO: rename once data is updated.
+  hpi2000;
+  /** This is weekly formula rent multiplied by 4.2 for a full month and adjusted for inflation (from the 1999 figures) as well as for bedroom size. */
+  adjustedSocialRentMonthly: number; 
+  /** Using `landToTotalRatio`, estimates how much of monthly social rent goes towards land. */
+  socialRentMonthlyLand: number;
+  /** Using `landToTotalRatio`, estimates how much of monthly social rent goes towards the house. */
+  socialRentMonthlyHouse: number;
   constructor(params: SocialRentParams) {
     this.countyAverageEarnings1999 = params.countyAverageEarnings1999;
     this.socialRentAdjustments = params.socialRentAdjustments;
-    this.averageMarketPrice2000 = params.averageMarketPrice2000;
+    this.hpi2000 = params.hpi2000;
     const {
       adjustedSocialRentMonthly,
       socialRentMonthlyLand,
@@ -39,7 +52,7 @@ export class SocialRent {
     const bedWeightsAndCaps = BED_WEIGHTS_AND_CAPS;
     const numberOfBedrooms = params.numberOfBedrooms;
 
-    const nationalAverageRent = NATIONAL_AVERAGES.averageRentWeekly;
+    const nationalAverageRent = NATIONAL_AVERAGES.rentWeekly;
     const nationalAverageMarketPrice1999 = NATIONAL_AVERAGES.propertyValue;
     const nationalAverageEarnings1999 = NATIONAL_AVERAGES.earningsWeekly;
 
@@ -58,7 +71,7 @@ export class SocialRent {
       this.countyAverageEarnings1999 / nationalAverageEarnings1999; // relative local earnings
 
     const relativePropertyValue =
-      this.averageMarketPrice2000 / nationalAverageMarketPrice1999; // relative property value; our local average market price is from 2000 because that's as far back as the HPI goes
+      this.hpi2000 / nationalAverageMarketPrice1999; // relative property value
 
     const formulaRentWeekly =
       0.7 * nationalAverageRent * relativeLocalEarning * bedWeight +
