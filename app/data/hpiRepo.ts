e4e5af0d30049ffe2ae18a90e4362cd9f6e604d2
Gabriele Granello <gabriele@opensystemslab.io>
2024-10-09 16:13:16 +0200

    hpiRepo test

--- app/data/hpiRepo.ts
+++ app/data/hpiRepo.ts
@@ -1,26 +1,31 @@
 import prisma from "./db";
 
 const getHPIByITL3 = async (itl3: string): Promise<number> => {
-    try {
-        const { 
-            _avg: { hpi2020: averageHpi },
-        } = await prisma.hPI.aggregate({
-            where: {
-                itl3: {
-                endsWith: itl3,
-                },
-            },
-            _avg: {
-                hpi2020: true,
-            },
-        });
-        return averageHpi as number;
-    } catch (error) {
-        throw Error(`Data error: Unable to find hpi2020 for itl3 ${itl3}`);
+  try {
+    const {
+      _avg: { hpi2020: averageHpi },
+    } = await prisma.hPI.aggregate({
+      where: {
+        itl3: {
+          endsWith: itl3,
+        },
+      },
+      _avg: {
+        hpi2020: true,
+      },
+    });
+
+    // Check if the average HPI is null and throw an error
+    if (averageHpi === null) {
+      throw new Error(`Data error: Unable to find hpi2020 for itl3 ${itl3}`);
     }
+
+    return averageHpi as number;
+  } catch (error) {
+    throw Error(`Data error: Unable to find hpi2020 for itl3 ${itl3}`);
+  }
 };
 
 export const hpi2020Repo = {
-    getHPIByITL3,
-  };
-  
+  getHPIByITL3,
+};
