4b2ca25a1e8e35ed2476fd4f42df6d9de37982d0
zzhhaa <zzhhaa@protonmail.com>
2024-11-22 16:56:30 +0000

    feat: add new test for updated depreciation

--- app/models/Property.test.ts
+++ app/models/Property.test.ts
@@ -24,8 +24,23 @@ it("correctly calculates the newBuildPrice", () => {
   expect(property.newBuildPrice).toBeCloseTo(186560);
 });
 
-it("correctly calculates the depreciatedBuildPrice", () => {
-  expect(property.depreciatedBuildPrice).toBeCloseTo(110717.45);
+it("correctly calculates the depreciatedBuildPrice for existing builds (age > 0)", () => {
+  expect(property.depreciatedBuildPrice).toBeCloseTo(172988.92);
+});
+
+it("correctly calculates depreciatedBuildPrice for newbuilds (age = 0)", () => {
+  property = new Property({
+    postcode: "WV8 1HG",
+    houseType: "T",
+    numberOfBedrooms: 2,
+    age: 1,
+    size: 88,
+    maintenancePercentage: 0.02,
+    newBuildPricePerMetre: 2120,
+    averageMarketPrice: 218091.58,
+    itl3: "TLG24",
+  });
+  expect(property.depreciatedBuildPrice).toBeCloseTo(186560)
 });
 
 it("correctly calculates the bedWeightedAveragePrice", () => {
