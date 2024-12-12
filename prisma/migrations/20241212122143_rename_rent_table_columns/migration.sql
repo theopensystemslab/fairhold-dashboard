617c467364f4db7dcdf2c439597182627f7aba83
zzhhaa <zzhhaa@protonmail.com>
2024-12-12 12:24:18 +0000

    feat: rename rent table and set fields required

---
+++ prisma/migrations/20241212122143_rename_rent_table_columns/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "rent" RENAME COLUMN "ladcode" TO "lad_code";
+ALTER TABLE "rent" RENAME COLUMN "monthlymeanrent" TO "monthly_mean_rent";
---
+++ prisma/migrations/20241212122332_make_rent_fields_required/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "rent" ALTER COLUMN "itl3" SET NOT NULL,
+ALTER COLUMN "lad_code" SET NOT NULL,
+ALTER COLUMN "region" SET NOT NULL,
+ALTER COLUMN "monthly_mean_rent" SET NOT NULL;
