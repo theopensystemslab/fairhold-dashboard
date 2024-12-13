2258e54648581aaa818924da35488d2fcd6178ad
Gabriele Granello <gabriele@opensystemslab.io>
2024-12-13 10:26:14 +0100

    add suspense block

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import CalculatorInput from "./components/ui/CalculatorInput";
+import { ClipLoader } from "react-spinners";
+import { Suspense } from "react";
 export default function Home() {
   return (
     <main>
-      <CalculatorInput />
+      <Suspense fallback={<ClipLoader />}>
+        <CalculatorInput />
+      </Suspense>
     </main>
   );
 }
