e402b4761e43bd48a30cdd083c2c28262a68331a
zzhhaa <zzhhaa@protonmail.com>
2024-12-18 11:26:25 +0000

    chore: rename getGDHIByITL3 function

--- app/services/gdhiService.ts
+++ app/services/gdhiService.ts
@@ -1,7 +1,7 @@
 import { gdhiRepo } from "../data/gdhiRepo";
 
 const getByITL3 = async (itl3: string) => {
-  return await gdhiRepo.getGDHI2020ByITL3(itl3);
+  return await gdhiRepo.getGDHIByITL3(itl3);
 };
 
 export const gdhiService = {
