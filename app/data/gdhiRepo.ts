3184e510bd59b7c355063abe186b76b940927542
zzhhaa <zzhhaa@protonmail.com>
2024-12-18 11:20:23 +0000

    feat: update Repo following column rename

--- app/data/gdhiRepo.ts
+++ app/data/gdhiRepo.ts
@@ -1,23 +1,23 @@
 import prisma from "./db";
 
-const getGDHI2020ByITL3 = async (itl3: string): Promise<number> => {
+const getGDHIByITL3 = async (itl3: string): Promise<number> => {
   try {
-    const { gdhi2020 } = await prisma.gDHI.findFirstOrThrow({
+    const { gdhi } = await prisma.gDHI.findFirstOrThrow({
       where: {
         AND: {
           itl3: { equals: itl3 },
         },
       },
-      select: { gdhi2020: true },
+      select: { gdhi: true },
     });
-
-    return gdhi2020;
+    console.log('Result:', gdhi); 
+    return gdhi;
   } catch (error) {
-    throw Error(`Data error: Unable to find gdhi2020 for itl3 ${itl3}`);
+    throw Error(`Data error: Unable to find gdhi for itl3 ${itl3}`);
   }
 };
 
 export const gdhiRepo = {
-  getGDHI2020ByITL3,
+  getGDHIByITL3,
 };
 
