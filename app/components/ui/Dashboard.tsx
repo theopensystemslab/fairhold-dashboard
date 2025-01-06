640587ab70a6feaaeed80acc720706612b5affb1
Gabriele Granello <gabriele@opensystemslab.io>
2025-01-06 18:37:57 +0100

    add accordion

--- app/components/ui/Dashboard.tsx
+++ app/components/ui/Dashboard.tsx
@@ -5,23 +5,27 @@ import LifetimeMarketPurchaseWrapper from "../graphs/LifetimeMarketPurchaseWrapp
 import LifetimeMarketRentWrapper from "../graphs/LifetimeMarketRentWrapper";
 import LifetimeFairholdLandPurchaseWrapper from "../graphs/LifetimeFairholdLandPurchaseWrapper";
 import LifetimeFairholdLandRentWrapper from "../graphs/LifetimeFairholdLandRentWrapper";
+import { formType } from "@/app/schemas/formSchema";
+import FormEdit from "./FormEdit";
 import { Household } from "@/app/models/Household";
 
 interface DashboardProps {
-  data: Household;
+  processedData: Household;
+  inputData: formType; // Add this property
 }
 
-const Dashboard: React.FC<DashboardProps> = ({ data }) => {
+const Dashboard: React.FC<DashboardProps> = ({ processedData, inputData }) => {
   return (
     <div>
       <h1>Dashboard</h1>
       {/* Render multiple graph components here */}
-      <TenureComparisonWrapper household={data} />
-      <TotalPaymentWrapper household={data} />
-      <LifetimeMarketPurchaseWrapper household={data} />
-      <LifetimeMarketRentWrapper household={data} />
-      <LifetimeFairholdLandPurchaseWrapper household={data} />
-      <LifetimeFairholdLandRentWrapper household={data} />{" "}
+      <FormEdit formData={inputData} />
+      <TenureComparisonWrapper household={processedData} />
+      <TotalPaymentWrapper household={processedData} />
+      <LifetimeMarketPurchaseWrapper household={processedData} />
+      <LifetimeMarketRentWrapper household={processedData} />
+      <LifetimeFairholdLandPurchaseWrapper household={processedData} />
+      <LifetimeFairholdLandRentWrapper household={processedData} />{" "}
     </div>
   );
 };
---
+++ app/components/ui/FormEdit.tsx
@@ -0,0 +1,57 @@
+"use client";
+import React from "react";
+import { formType } from "@/app/schemas/formSchema";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { HouseType } from "../../models/Property";
+
+interface Props {
+  formData: formType;
+}
+
+function mapHouseType(houseType: HouseType) {
+  if (houseType === "D") {
+    return "Detached";
+  }
+
+  if (houseType === "S") {
+    return "Semi-detached";
+  }
+
+  if (houseType === "T") {
+    return "Terrace";
+  }
+
+  if (houseType === "F") {
+    return "Flat";
+  }
+}
+
+const FormEdit: React.FC<Props> = ({ formData }) => {
+  return (
+    <Accordion type="single" collapsible className="inline-block">
+      <AccordionItem value="item-1">
+        <div className="flex space-x-4 items-center bg-black  px-2">
+          <AccordionTrigger>EDIT</AccordionTrigger>
+          <span>{mapHouseType(formData.houseType)}</span>
+          <span>
+            {formData.houseSize} m<sup>2</sup>
+          </span>
+          <span>{formData.housePostcode}</span>
+          <span>{formData.houseAge} years old</span>
+          <span>{formData.houseBedrooms} bedrooms</span>
+          <span>{formData.maintenancePercentage * 100}% maintenance</span>
+        </div>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+export default FormEdit;
