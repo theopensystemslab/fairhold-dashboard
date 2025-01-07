282ec2e843b33d793767aab342601114cb8524b3
zzhhaa <zzhhaa@protonmail.com>
2025-01-07 09:52:30 +0000

    test: add tenure class test to Household

--- app/models/Household.test.ts
+++ app/models/Household.test.ts
@@ -174,3 +174,16 @@ beforeEach(() => {
 it("can be instantiated", () => {
   expect(household).toBeInstanceOf(Household);
 });
+
+it("contains all tenure classes", () => {
+  expect(household).toHaveProperty("tenure"),
+  expect(household.tenure).toHaveProperty("marketPurchase"),
+  expect(household.tenure).toHaveProperty("marketRent"),
+  expect(household.tenure).toHaveProperty("socialRent"),
+  expect(household.tenure).toHaveProperty("fairholdLandPurchase"),
+  expect(household.tenure).toHaveProperty("fairholdLandRent")
+})
+
+it("contains an instance of Lifetime", () => {
+  expect(household).toHaveProperty("lifetime")
+})
