cf30d0434a785a6df8894cf04c579b9af71abcef
zzhhaa <zzhhaa@protonmail.com>
2025-01-09 17:12:44 +0000

    chore: remove console logs

--- app/models/tenure/FairholdLandRent.test.ts
+++ app/models/tenure/FairholdLandRent.test.ts
@@ -1,6 +1,6 @@
 import { Mortgage } from "../Mortgage";
 import { FairholdLandRent } from "./FairholdLandRent";
-import { createTestFairholdLandRent, createTestMarketPurchase } from "../testHelpers";
+import { createTestFairholdLandRent } from "../testHelpers";
 
 const fairholdLandRent = createTestFairholdLandRent();
 
@@ -13,14 +13,10 @@ it("instantiates an instance of Mortgage", () => {
 })
 
 it("correctly calculates discountedLandRentMonthly", () => {
-  console.log({fairholdLandRent})
   expect(fairholdLandRent.discountedLandRentMonthly).toBeCloseTo(135); // TODO: check these calcs, I tried manually calculating this and I keep getting 180
 })
 
 it("correctly calculates interestPaid", () => {
-  const marketPurchase = createTestMarketPurchase();
-  console.log({marketPurchase})
-  console.log({fairholdLandRent})
   expect(fairholdLandRent.interestPaid).toBeCloseTo(147693.69);
 })
 
