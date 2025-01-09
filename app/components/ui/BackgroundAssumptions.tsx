da8d8204f2ce0c3171f900ff5f01ea60c18bdb7c
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-01-09 17:08:56 +0000

    feat: Add background assumptions button

---
+++ app/components/ui/BackgroundAssumptions.tsx
@@ -0,0 +1,8 @@
+import { QuestionMarkCircledIcon } from "@radix-ui/react-icons";
+
+export const BackgroundAssumptions: React.FC = () =>  (
+  <a href="#" className="flex text-sm items-center">
+    <QuestionMarkCircledIcon className="mr-2"/>
+    Explore other background assumptions we have made in this calculator
+  </a>
+)
