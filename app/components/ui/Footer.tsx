25c4a5a08dc304f703bc5184f36992bfc392241d
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-01-09 10:27:30 +0000

    fix: Tidy ups and tweaks

--- app/components/ui/Footer.tsx
+++ app/components/ui/Footer.tsx
@@ -1,7 +1,9 @@
+import "./../../webflow.css";
+
 import Image from "next/image";
 
 export const Footer: React.FC = () => (
-  <section className="footer">
+  <footer className="footer mt-5">
     <div className="w-layout-blockcontainer container w-container">
       <div className="columns w-row">
         <div className="column-6 w-col w-col-3"><a href="#" className="footer-link-block w-inline-block">
@@ -50,7 +52,7 @@ export const Footer: React.FC = () => (
               src="https://cdn.prod.website-files.com/665dd5e4ad0dc9aad281585c/6754893e21b56d4557aa3597_OSL_icon_white.svg"
               loading="lazy" width="40" height="40" alt="OSL-logo"/>
             <div className="spacer _10"></div>
-            <div className="osl-text _20">Open Systems Lab </div>
+            <div className="osl-text _20">Open<br/>Systems<br/>Lab</div>
           </a>
           <div className="spacer"></div>
         </div>
@@ -60,20 +62,20 @@ export const Footer: React.FC = () => (
           <div className="funders-logos-wrapper"><a href="#" className="logo-link w-inline-block">
             <Image
             src="https://cdn.prod.website-files.com/665dd5e4ad0dc9aad281585c/6754910ec9989275efe70268_TNCLFlogo.svg"
-            loading="lazy" height="60" width="100" alt="The National Lottery Community Fund Logo"/></a>
+            loading="lazy" height="60" width="179" alt="The National Lottery Community Fund Logo"/></a>
             <div className="spacer _30"></div><a href="#" className="logo-link w-inline-block">
               <Image
               src="https://cdn.prod.website-files.com/665dd5e4ad0dc9aad281585c/6754907fcfae984354b4cf6c_JRFlogo2024.svg"
-                loading="lazy" height="62" alt="JRF logo" width="100"/></a>
+                loading="lazy" height="62" alt="JRF logo" width="62"/></a>
           </div>
         </div>
         <div className="column-5 w-col w-col-3">
           <div className="body-s white">© Open Systems
-            Lab 2025 Open Systems Lab / OSL is a non-profit company 9152368
+            Lab 2025 <br/><br/>Open Systems Lab / OSL is a non-profit company 9152368<br/>
             registered in England &amp; Wales
           </div>
         </div>
       </div>
     </div>
-  </section>
+  </footer>
 )
