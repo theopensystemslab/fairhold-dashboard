2e572428b8f5e7296015ec4dc958356da878de39
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-01-09 20:46:13 +0000

    feat: Styles and dot icons

--- app/components/ui/Carousel.tsx
+++ app/components/ui/Carousel.tsx
@@ -1,4 +1,5 @@
-import { ArrowDownIcon, ArrowUpIcon } from "@radix-ui/react-icons";
+import { cn } from "@/lib/utils";
+import { ArrowDownIcon, ArrowUpIcon, DotFilledIcon } from "@radix-ui/react-icons";
 import { IconProps } from "@radix-ui/react-icons/dist/types";
 import { ForwardRefExoticComponent, RefAttributes } from "react";
 
@@ -9,17 +10,16 @@ interface NavigationIconProps {
 
 const NavigationIcon: React.FC<NavigationIconProps> = ({ Icon, onClick }) => (
   <button onClick={onClick}>
-    <Icon className="w-10 h-10 hover:scale-125 transition-transform duration-200" />
+    <Icon className="w-6 h-6 hover:scale-125 transition-transform duration-200" />
   </button>
 );
 
 interface CarouselProps {
   scrollContainerRef: React.RefObject<HTMLDivElement>;
   currentPage: number;
-  setCurrentPage: React.Dispatch<React.SetStateAction<number>>
 }
 
-export const Carousel: React.FC<CarouselProps> = ({ scrollContainerRef, currentPage, setCurrentPage }) => {
+export const Carousel: React.FC<CarouselProps> = ({ scrollContainerRef, currentPage }) => {
   const totalPages = 5;
 
   const handleNext = () => {
@@ -29,7 +29,6 @@ export const Carousel: React.FC<CarouselProps> = ({ scrollContainerRef, currentP
         behavior: "smooth",
       });
     }
-    setCurrentPage(currentPage + 1);
   };
 
   const handlePrevious = () => {
@@ -39,25 +38,30 @@ export const Carousel: React.FC<CarouselProps> = ({ scrollContainerRef, currentP
         behavior: "smooth",
       });
     }
-    setCurrentPage(currentPage - 1);
   };
 
   return (
-    <>
-      {
-        currentPage > 0 && (
-          <div className="fixed top-4 right-4">
-            <NavigationIcon Icon={ArrowUpIcon} onClick={handlePrevious}/>
-          </div>
-        )
-      }
-      {
-        currentPage < totalPages && (
-          <div className="fixed bottom-4 right-4">
-            <NavigationIcon Icon={ArrowDownIcon} onClick={handleNext} />
-          </div>
-        )
-      }
-    </>
+    <div className="flex flex-col justify-between fixed right-4 top-0 h-full items-center py-6">
+      <div className={cn("invisible", { "visible": currentPage > 0 })}>
+        <NavigationIcon Icon={ArrowUpIcon} onClick={handlePrevious} />
+      </div>
+      <div className="flex flex-col items-center">
+        {Array.from({ length: totalPages + 1 }, (_, index) => (
+          <button key={index} onClick={() => console.log("todo - scroll")}>
+            <DotFilledIcon 
+              className={cn(
+                "text-gray-300 w-8 h-8 -m-2", 
+                { "text-gray-950": 
+                  index === currentPage 
+                }
+              )}
+            />
+          </button>
+        ))}
+      </div>
+      <div className={cn("invisible", { "visible": currentPage < totalPages })}>
+        <NavigationIcon Icon={ArrowDownIcon} onClick={handleNext} />
+      </div>
+    </div>
   )
 }
