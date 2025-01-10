04b8f1c495460c9346ea208a78366bdde6a21aa5
Gabriele Granello <gabriele@opensystemslab.io>
2025-01-10 13:00:12 +0100

    wip initial cards

---
+++ app/components/graphs/TenureComparisonBarChart.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+const chartConfig = {
+  land: {
+    label: "Land",
+    color: "hsl(var(--chart-1))",
+  },
+  house: {
+    label: "House",
+    color: "hsl(var(--chart-2))",
+  },
+} satisfies ChartConfig;
+
+import React from "react";
+
+// Define type for DataInput
+type DataInput = {
+  category: string;
+  marketPurchase: number;
+  marketRent: number;
+  socialRent: number;
+  fairholdLandPurchase: number;
+  fairholdLandRent: number;
+  affordabilityMonthly: number;
+  [key: string]: string | number;
+};
+
+// Define the props type for StackedBarChart
+interface StackedBarChartProps {
+  data: DataInput[];
+}
+
+// Implementation of the Chart.js Stacked Bar Chart
+const TenureComparisonBarChart: React.FC<StackedBarChartProps> = ({ data }) => {
+  const chartData = [
+    {
+      tenure: "market: purchase",
+      land: data[0].marketPurchase,
+      house: data[1].marketPurchase,
+    },
+    {
+      tenure: "market: rent",
+      land: data[0].marketRent,
+      house: data[1].marketRent,
+    },
+    {
+      tenure: "social rent",
+      land: data[0].socialRent,
+      house: data[1].socialRent,
+    },
+    {
+      tenure: "fairhold: land purchase",
+      land: data[0].fairholdLandPurchase,
+      house: data[1].fairholdLandPurchase,
+    },
+    {
+      tenure: "fairhold: land rent",
+      land: data[0].fairholdLandRent,
+      house: data[1].fairholdLandRent,
+    },
+  ];
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Tenure comparison</CardTitle>
+        <CardDescription>Monthly cost in £</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="tenure"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value}
+            />
+            <ChartTooltip content={<ChartTooltipContent hideLabel />} />
+            <ChartLegend content={<ChartLegendContent />} />
+            <Bar
+              dataKey="land"
+              stackId="a"
+              fill="var(--color-land)"
+              radius={[0, 0, 4, 4]}
+            />
+            <Bar
+              dataKey="house"
+              stackId="a"
+              fill="var(--color-house)"
+              radius={[4, 4, 0, 0]}
+            />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm">
+        <div className="leading-none text-muted-foreground">
+          Showing the montly cost by tenure type.
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default TenureComparisonBarChart;
