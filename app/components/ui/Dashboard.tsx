f9e00eb51dd6280b18a7960b4ab119586cf18d91
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-01-10 12:22:43 +0000

    feat: Scroll page indicator (#266)
    * feat: Button with previous card functionality
    
    * feat: Extract carousel to it's own component
    
    * feat: Styles and dot icons
    
    * feat: Scroll button, simplify next and prev functions
    
    * fix: Absolute not fixed

--- app/components/ui/Dashboard.tsx
+++ app/components/ui/Dashboard.tsx
@@ -5,6 +5,7 @@ import { FormFrontend } from "@/app/schemas/formSchema";
 import { WhatWouldYouChoose } from "../Dashboard/Cards/WhatWouldYouChoose";
 import { WhatDifference } from "../Dashboard/Cards/WhatDifference";
 import { HowMuchFHCost } from "../Dashboard/Cards/HowMuchFHCost";
+import { Carousel } from "./Carousel";
 
 interface DashboardProps {
   processedData: Household;
@@ -14,17 +15,6 @@ interface DashboardProps {
 const Dashboard: React.FC<DashboardProps> = ({ inputData, processedData }) => {
   const scrollContainerRef = useRef<HTMLDivElement>(null);
   const [currentPage, setCurrentPage] = useState(0);
-  const totalPages = 6;
-
-  const handleNext = () => {
-    if (scrollContainerRef.current) {
-      scrollContainerRef.current.scrollBy({
-        top: window.innerHeight,
-        behavior: "smooth",
-      });
-    }
-    setCurrentPage((prev) => Math.min(prev + 1, totalPages - 1));
-  };
 
   const handleScroll = () => {
     if (scrollContainerRef.current) {
@@ -46,7 +36,6 @@ const Dashboard: React.FC<DashboardProps> = ({ inputData, processedData }) => {
         onScroll={handleScroll}
       >
         <HowMuchFHCost data={processedData} />
-
         <GraphCard title="How much would it cost every month?">
           <span className="text-red-500">in theory less than Freehold</span>
         </GraphCard>
@@ -55,16 +44,10 @@ const Dashboard: React.FC<DashboardProps> = ({ inputData, processedData }) => {
         <WhatDifference />
         <WhatWouldYouChoose />
       </div>
-      {currentPage < totalPages - 1 && (
-        <div className="fixed bottom-4 right-4">
-          <button
-            onClick={handleNext}
-            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          >
-            Next
-          </button>
-        </div>
-      )}
+      <Carousel
+        scrollContainerRef={scrollContainerRef}
+        currentPage={currentPage}
+      />
     </div>
   );
 };
