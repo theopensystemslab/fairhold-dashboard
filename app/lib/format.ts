093b617f2b51feaa3e16d34acdd016244a65f46a
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-01-10 14:31:26 +0000

    feat: Add labels to bar chart

---
+++ app/lib/format.ts
@@ -0,0 +1,12 @@
+/**
+ * Convert a number to a human readable value in pounds
+ */
+export const formatValue = (value: number) => {
+  if (value >= 1000000) {
+    return `£${(value / 1000000).toFixed(1)}M`;
+  }
+  if (value >= 1000) {
+    return `£${(value / 1000).toFixed(0)}K`;
+  }
+  return `£${value}`;
+};
