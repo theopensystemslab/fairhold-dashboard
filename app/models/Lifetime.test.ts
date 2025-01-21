4ee1cd6d7af7b8c48346c1dfaec8fca718b26c34
zzhhaa <zzhhaa@protonmail.com>
2025-01-21 16:05:01 +0000

    feat: new property storage objects in Lifetime for maintenanceCost and depreciatedHouseResaleValue

--- app/models/Lifetime.test.ts
+++ app/models/Lifetime.test.ts
@@ -28,10 +28,10 @@ describe("resale values", () => {
         lifetime = createTestLifetime({
             property: createTestProperty({ age: 0 })
         });
-        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValueNoMaintenance).toBe(186560);
-        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValueLowMaintenance).toBe(186560);
-        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValueMediumMaintenance).toBe(186560);
-        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValueHighMaintenance).toBe(186560);
+        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValue.none).toBe(186560);
+        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValue.low).toBe(186560);
+        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValue.medium).toBe(186560);
+        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValue.high).toBe(186560);
 
     });
     it("correctly calculates for a 10-year old house", () => {    // Test 10-year-old house
@@ -49,32 +49,32 @@ describe("resale values", () => {
             size: 88 
         })           
         const depreciatedHouseY10 = houseY10.calculateDepreciatedBuildPrice()
-        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValueLowMaintenance).toBe(depreciatedHouseY10);
+        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValue.low).toBe(depreciatedHouseY10);
     });
     it("depreciates the house over time", () => {
         // Test value changes over time
-        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValueNoMaintenance).toBeGreaterThan(
-            lifetime.lifetimeData[10].depreciatedHouseResaleValueNoMaintenance);
-        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValueLowMaintenance).toBeGreaterThan(
-            lifetime.lifetimeData[10].depreciatedHouseResaleValueLowMaintenance);
-        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValueMediumMaintenance).toBeGreaterThan(
-            lifetime.lifetimeData[10].depreciatedHouseResaleValueMediumMaintenance);
+        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValue.none).toBeGreaterThan(
+            lifetime.lifetimeData[10].depreciatedHouseResaleValue.none);
+        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValue.low).toBeGreaterThan(
+            lifetime.lifetimeData[10].depreciatedHouseResaleValue.low);
+        expect(lifetime.lifetimeData[0].depreciatedHouseResaleValue.medium).toBeGreaterThan(
+            lifetime.lifetimeData[10].depreciatedHouseResaleValue.medium);
         // depreciatedHouseResaleValueHighMaintenance isn't tested here because the addition of a major work causes price to rise
     })
 });
 
 it("correctly calculates depreciated house value for all maintenance levels", () => {
-    expect(lifetime.lifetimeData[3].depreciatedHouseResaleValueNoMaintenance).toBeCloseTo(173636.99)
-    expect(lifetime.lifetimeData[20].depreciatedHouseResaleValueLowMaintenance).toBeCloseTo(157202.92)
-    expect(lifetime.lifetimeData[27].depreciatedHouseResaleValueMediumMaintenance).toBeCloseTo(168197.27)
-    expect(lifetime.lifetimeData[39].depreciatedHouseResaleValueHighMaintenance).toBeCloseTo(203777.25) 
+    expect(lifetime.lifetimeData[3].depreciatedHouseResaleValue.none).toBeCloseTo(173636.99)
+    expect(lifetime.lifetimeData[20].depreciatedHouseResaleValue.low).toBeCloseTo(157202.92)
+    expect(lifetime.lifetimeData[27].depreciatedHouseResaleValue.medium).toBeCloseTo(168197.27)
+    expect(lifetime.lifetimeData[39].depreciatedHouseResaleValue.high).toBeCloseTo(203777.25) 
 
-    expect(lifetime.lifetimeData[5].depreciatedHouseResaleValueNoMaintenance).toBeLessThan(
-        lifetime.lifetimeData[5].depreciatedHouseResaleValueLowMaintenance);
-    expect(lifetime.lifetimeData[5].depreciatedHouseResaleValueLowMaintenance).toBeLessThan(
-        lifetime.lifetimeData[5].depreciatedHouseResaleValueMediumMaintenance);
-    expect(lifetime.lifetimeData[5].depreciatedHouseResaleValueMediumMaintenance).toBeLessThan(
-        lifetime.lifetimeData[5].depreciatedHouseResaleValueHighMaintenance);
+    expect(lifetime.lifetimeData[5].depreciatedHouseResaleValue.none).toBeLessThan(
+        lifetime.lifetimeData[5].depreciatedHouseResaleValue.low);
+    expect(lifetime.lifetimeData[5].depreciatedHouseResaleValue.low).toBeLessThan(
+        lifetime.lifetimeData[5].depreciatedHouseResaleValue.medium);
+    expect(lifetime.lifetimeData[5].depreciatedHouseResaleValue.medium).toBeLessThan(
+        lifetime.lifetimeData[5].depreciatedHouseResaleValue.high);
 })
 
 it("correctly ages the house", () => {
