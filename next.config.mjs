0a18387c68a2810603fb1baf257f9f69a2c2c35c
zzhhaa <zzhhaa@protonmail.com>
2025-01-28 15:26:48 +0000

    feat: install react-markdown

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => { 
+    config.module.rules.push({ 
+      test: /\.md$/, 
+      type: 'asset/source', 
+    }); 
+    return config; 
+  }, 
 };
 
 export default nextConfig;
