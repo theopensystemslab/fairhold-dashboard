134d491bb1f65abce93ec05cb229f519032f17b1
zzhhaa <zzhhaa@protonmail.com>
2025-01-29 13:26:26 +0000

    nit: lower case values for copy consistency

--- app/lib/format.ts
+++ app/lib/format.ts
@@ -3,10 +3,10 @@
  */
 export const formatValue = (value: number) => {
   if (value >= 1000000) {
-    return `£${(value / 1000000).toFixed(1)}M`;
+    return `£${(value / 1000000).toFixed(1)}m`;
   }
   if (value >= 10000) {
-    return `£${(value / 1000).toFixed(0)}K`;
+    return `£${(value / 1000).toFixed(0)}k`;
   }
   return `£${value.toFixed(0)}`;
 };
