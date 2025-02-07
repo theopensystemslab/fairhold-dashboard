950cd3b262a003a17a07971220f946ff5705a3ba
Gabriele Granello <52770098+gabrielegranello@users.noreply.github.com>
2025-02-07 17:03:22 +0100

    feat: optional floor area (#317)
    * rename test classes file
    
    * fix missing reference
    
    * optional floor area in the form validation
    
    * fixes to form schemas
    
    * removed optional
    
    * add optional
    
    * use transform
    
    * added optional to the frontend
    
    ---------
    
    Co-authored-by: Gabriele Granello <gabriele@opensystemslab.io>

--- app/schemas/calculationSchema.ts
+++ app/schemas/calculationSchema.ts
@@ -1,11 +1,6 @@
 import { z } from "zod";
-import {
-  parse as parsePostcode,
-  isValid as isValidPostcode,
-} from "postcode";
-import { 
-  HOUSE_TYPES 
-} from "../models/Property";
+import { parse as parsePostcode, isValid as isValidPostcode } from "postcode";
+import { HOUSE_TYPES } from "../models/Property";
 import { MAINTENANCE_LEVELS } from "../models/constants";
 
 export type ValidPostcode = Extract<
@@ -14,30 +9,53 @@ export type ValidPostcode = Extract<
 >;
 
 const HouseTypeEnum = z.enum(HOUSE_TYPES);
+function assignHouseSize(numberOfBedrooms: number) {
+  const sizeMapping: { [key: number]: number } = {
+    1: 55,
+    2: 70,
+    3: 95,
+    4: 110,
+    5: 125,
+    6: 135,
+  };
 
-export const maintenanceLevelSchema = z.enum(Object.keys(MAINTENANCE_LEVELS) as [keyof typeof MAINTENANCE_LEVELS, ...Array<keyof typeof MAINTENANCE_LEVELS>]);
+  const size = numberOfBedrooms > 6 ? 135 : sizeMapping[numberOfBedrooms];
+  return size;
+}
 
-export const calculationSchema = z.object({
-  housePostcode: z
-    .string()
-    .min(1, "housePostcode is required")
-    .refine(isValidPostcode, "Invalid postcode")
-    .transform(parsePostcode)
-    .refine((postcode): postcode is ValidPostcode => postcode.valid),
-  houseSize: z.coerce.number().positive("houseSize must be a positive integer"),
-  houseAge: z.coerce
-    .number()
-    .nonnegative("houseAge must be a positive integer or 0"),
-  houseBedrooms: z.coerce
-    .number()
-    .positive("houseBedrooms must be a positive integer"),
-  houseType: HouseTypeEnum.refine(
-    (value) => HouseTypeEnum.options.includes(value),
-    {
-      message: `houseType is required and must be one of ${HOUSE_TYPES}`,
-    }
-  ),
-  maintenanceLevel: maintenanceLevelSchema,
-});
+export const maintenanceLevelSchema = z.enum(
+  Object.keys(MAINTENANCE_LEVELS) as [
+    keyof typeof MAINTENANCE_LEVELS,
+    ...Array<keyof typeof MAINTENANCE_LEVELS>,
+  ]
+);
+
+export const calculationSchema = z
+  .object({
+    housePostcode: z
+      .string()
+      .min(1, "housePostcode is required")
+      .refine(isValidPostcode, "Invalid postcode")
+      .transform(parsePostcode)
+      .refine((postcode): postcode is ValidPostcode => postcode.valid),
+    houseBedrooms: z.coerce
+      .number()
+      .positive("houseBedrooms must be a positive integer"),
+    houseSize: z.coerce.number().optional(),
+    houseAge: z.coerce
+      .number()
+      .nonnegative("houseAge must be a positive integer or 0"),
+    houseType: HouseTypeEnum.refine(
+      (value) => HouseTypeEnum.options.includes(value),
+      {
+        message: `houseType is required and must be one of ${HOUSE_TYPES}`,
+      }
+    ),
+    maintenanceLevel: maintenanceLevelSchema,
+  })
+  .transform((data) => ({
+    ...data,
+    houseSize: data.houseSize ?? assignHouseSize(data.houseBedrooms),
+  }));
 
 export type Calculation = z.infer<typeof calculationSchema>;
