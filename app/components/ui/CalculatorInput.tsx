57913647ad9c1a3ebd95dd16aa052f50978d7b3d
Gabriele Granello <gabriele@opensystemslab.io>
2025-03-07 11:33:36 +0100

    restructre the grid

--- app/components/ui/CalculatorInput.tsx
+++ app/components/ui/CalculatorInput.tsx
@@ -197,7 +197,7 @@ const CalculatorInput = () => {
                 </FormItem>
               )}
             />
-            <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               <FormField
                 control={form.control}
                 name="housePostcode"
