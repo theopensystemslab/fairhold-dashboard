a73328f17c2ba16374a460400b7ad9f694bca4b5
zzhhaa <zzhhaa@protonmail.com>
2025-03-28 12:37:00 +0000

    feat: add link to wiki docs

--- app/components/ui/BackgroundAssumptions.tsx
+++ app/components/ui/BackgroundAssumptions.tsx
@@ -1,7 +1,7 @@
 import { QuestionMarkCircledIcon } from "@radix-ui/react-icons";
 
 export const BackgroundAssumptions: React.FC = () =>  (
-  <a href="#" className="flex text-sm items-center">
+  <a href="https://github.com/theopensystemslab/fairhold-dashboard/wiki" className="flex text-sm items-center">
     <QuestionMarkCircledIcon className="mr-2"/>
     Explore other background assumptions we have made in this calculator
   </a>
