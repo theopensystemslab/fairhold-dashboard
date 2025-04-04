6b5e1c0abbd520a1d39ac395ad5ef55f63f7c940
zzhhaa <zzhhaa@protonmail.com>
2025-04-04 13:13:36 +0100

    feat: add fairhold percentage in subtitle for upfront costs

--- app/components/Dashboard/Cards/HowMuchFHCost.tsx
+++ app/components/Dashboard/Cards/HowMuchFHCost.tsx
@@ -10,12 +10,23 @@ interface DashboardProps {
 }
 
 export const HowMuchFHCost: React.FC<DashboardProps> = ({ data }) => {
+  const fairholdPercentage = Math.round((data.tenure.fairholdLandPurchase.discountedLandPrice + data.property.depreciatedBuildPrice) // shows FairholdLP
+    / data.property.averageMarketPrice * 100)
+
   return (
     <GraphCard
-      title="How much would a Fairhold home cost me?"
-      subtitle="The up-front cost of a home, compared with conventional home ownership"
-    >
-      <div className="flex flex-col h-full md:w-3/4 w-full justify-between">
+      title="How much would a home cost?"
+      subtitle={
+              <span>
+                A Fairhold home could cost 
+                <span style={{ color: "rgb(var(--fairhold-equity-color-rgb))" }} className="font-semibold">
+                  {` ${fairholdPercentage}% `}
+                </span>
+                of its freehold price.
+              </span>
+            }
+          >
+      <div className="flex flex-col h-full w-3/4 justify-between">
         <HowMuchFHCostWrapper household={data} />
         <Drawer
           buttonTitle="Find out more about how we estimated these"
