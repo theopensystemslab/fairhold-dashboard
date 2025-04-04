de08c0bea4e6987e69a76366423336c6ff60a9bb
zzhhaa <zzhhaa@protonmail.com>
2025-04-04 13:12:50 +0100

    feat: add upfront cost tooltip

--- app/components/graphs/HowMuchFHCostBarChart.tsx
+++ app/components/graphs/HowMuchFHCostBarChart.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import { Bar, BarChart, CartesianGrid, XAxis, Label, LabelList } from "recharts";
+import { Bar, BarChart, CartesianGrid, XAxis, Label, LabelList, Tooltip } from "recharts";
+import type { ValueType, NameType } from "recharts/types/component/DefaultTooltipContent";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import {
   ChartConfig,
 } from "@/components/ui/chart";
+import { TooltipProps } from "recharts";
 import {
   StyledChartContainer,
 } from "../ui/StyledChartContainer";
+
+import React from "react";
+import { formatValue } from "@/app/lib/format";
 
 const chartConfig = {
   freeholdLand: {
@@ -27,8 +32,43 @@ const chartConfig = {
   }
 } satisfies ChartConfig;
 
-import React from "react";
-import { formatValue } from "@/app/lib/format";
+const UpFrontCostTooltip = ({ active, payload }: TooltipProps<ValueType, NameType>) => {
+  if (!active || !payload) return null;
+
+    // Sort payload so House comes before Land
+    const sortedPayload = [...payload].sort((a, b) => {
+      const aIsLand = (a.dataKey as string).includes("Land");
+      const bIsLand = (b.dataKey as string).includes("Land");
+      return aIsLand ? 1 : bIsLand ? -1 : 0;
+    });
+
+  return (
+    <div className="rounded-lg bg-[rgb(var(--text-default-rgb))] p-2 shadow-sm">
+      <div className="flex flex-col gap-2">
+        {sortedPayload.map((segment, index) => {
+          const dataKey = segment.dataKey as string;
+          const value = segment.value as number;
+
+          let segmentLabel = "";
+          if (dataKey.includes("House")) {
+            segmentLabel = "House";
+          }
+          else if (dataKey.includes("Land")) {
+            segmentLabel = "Land";
+          }
+          return (
+            <div key={index} className="grid grid-cols-2 gap-2">
+              <div className="font-normal text-white">
+                {`${segmentLabel}`}
+              </div>
+              <div className="text-right text-white">{formatValue(value)}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 type DataInput = {
   category: string;
@@ -180,6 +220,7 @@ const HowMuchFHCostBarChart: React.FC<StackedBarChartProps> = ({
                 fontWeight={600}
                 />
             </Bar>
+            <Tooltip content={<UpFrontCostTooltip />} />
           </BarChart>
         </StyledChartContainer>
       </CardContent>
