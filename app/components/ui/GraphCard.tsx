9ca6ef8a5845d3a8a1c4a4aedaa0db1d4386d069
zzhhaa <zzhhaa@protonmail.com>
2025-04-11 17:28:24 +0100

    feat: style graph subtitle weight

--- app/components/ui/GraphCard.tsx
+++ app/components/ui/GraphCard.tsx
@@ -10,7 +10,7 @@ const GraphCard: React.FC<Props> = ({ title, subtitle, children }) => {
     <div className="h-screen snap-start p-10 flex flex-col">
       <div className="w-full md:w-3/4">
         <h1 className={`text-2xl md:text-3xl lg:text-4xl  sm:text-xl font-bold text-black`}>{title}</h1>
-        {subtitle && <h2 className={`text-lg md:text-xl lg:text-2xl text-gray-600 mt-2`}>{subtitle}</h2>}
+        {subtitle && <h2 className={`text-lg md:text-xl lg:text-2xl text-gray-600 mt-2 font-normal`}>{subtitle}</h2>}
       </div>
       {children && <div className="mt-4 flex-1">{children}</div>}
     </div>
