2bbbef4b2d5b018edfcaf473748e26e545a9a2d7
zzhhaa <zzhhaa@protonmail.com>
2025-04-23 14:40:30 +0100

    fix: setup survey results page and route

--- app/survey/page.tsx
+++ app/survey/page.tsx
@@ -3,12 +3,14 @@
 import { useState, useEffect } from 'react';
 import ErrorBoundary from '../components/ErrorBoundary';
 import { inter } from '../page';
+import { SurveyResults } from './types';
 
 // list records https://api.airtable.com/v0/{baseId}/{tableIdOrName}
 // get record https://api.airtable.com/v0/{baseId}/{tableIdOrName}/{recordId}
 
 
 export default function SurveyPage() {
+  const [surveyData, setSurveyData] = useState<SurveyResults[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
@@ -22,6 +24,7 @@ export default function SurveyPage() {
         }
         
         const data = await response.json();
+        setSurveyData(data.results);
         console.log(data)
       } catch (err) {
         setError('Failed to fetch survey data');
@@ -40,11 +43,25 @@ export default function SurveyPage() {
   return (
     <ErrorBoundary>
         <main className={inter.className}>
-            <div className="container mx-auto py-8">
-                <h1 className="h1-style">Survey Results</h1>
-                <div className="grid gap-4 mt-6">
-                </div>
+          <div className="flex flex-col items-center justify-center h-screen bg-[rgb(var(--background-end-rgb))]">
+            <h1 className="h1-style">Survey Results</h1>
+              <div className="flex flex-col gap-4 mt-6">
+              {surveyData.length === 0 ? (
+            <p>No survey responses found.</p>
+          ) : (
+            <div>
+            <div className="flex flex-col gap-2">
+                <h2 className="h2-style">Survey Question 1</h2>
+                <p>Survey Answer 1</p>
             </div>
+            <div className="flex flex-col gap-2">
+                <h2 className="h2-style">Survey Question 2</h2>
+                <p>Survey Answer 2</p>
+            </div>
+          </div>
+          )}
+            </div>
+          </div>
         </main>
     </ErrorBoundary>
   );
