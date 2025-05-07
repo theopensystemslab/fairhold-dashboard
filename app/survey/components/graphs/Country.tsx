d0ff2f5c3eb854c91b1fe6a5171a8f740d7b2047
zzhhaa <zzhhaa@protonmail.com>
2025-05-07 14:52:57 +0100

    feat: country pie chart

--- app/survey/components/graphs/Country.tsx
+++ app/survey/components/graphs/Country.tsx
@@ -1,9 +1,16 @@
 import React from "react"
 import { SurveyComponentProps } from "@/app/survey/types";
 import SurveyGraphCard from "@/app/survey/components/SurveyGraphCard";
+import { PieChart, Pie, Legend } from "recharts";
 
 export const Country: React.FC<SurveyComponentProps> = ({ results }) => {
+    console.log(results.country)
     return (
-        <SurveyGraphCard title="Which country?" results={results}></SurveyGraphCard>
+        <SurveyGraphCard title="Which country?" results={results}>
+            <PieChart>
+                <Pie data={results.country} dataKey="value" nameKey="name" fill="#8884d8" />
+                <Legend align="center" verticalAlign="bottom" height={36}/>
+            </PieChart>
+        </SurveyGraphCard>
     )
 }
