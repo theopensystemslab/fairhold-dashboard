ebf9d06011180b4241b79ab2425cd18e07a6e646
zzhhaa <zzhhaa@protonmail.com>
2025-05-07 14:50:20 +0100

    feat: wrap graph in ResponsiveContainer

--- app/survey/components/SurveyGraphCard.tsx
+++ app/survey/components/SurveyGraphCard.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { ReactElement } from "react";
+import { ResponsiveContainer } from "recharts";
 import { FormattedSurveyResults } from "@/app/survey/types";
 
 type Props = React.PropsWithChildren<{
   title: string;
   subtitle?: React.ReactNode;
   results: FormattedSurveyResults;
+  children?: ReactElement;
 }>;
 
 const SurveyGraphCard: React.FC<Props> = ({ title, subtitle, children }) => {
@@ -12,7 +14,15 @@ const SurveyGraphCard: React.FC<Props> = ({ title, subtitle, children }) => {
     <div className="justify-center w-full bg-white m-4 p-4">
         <h3 className={`text-xl md:text-lg sm:text-md font-normal text-black`}>{title}</h3>
         {subtitle && <p className={`text-md md:text-lg text-gray-600 mt-2 font-normal`}>{subtitle}</p>}
-      {children && <div className="mt-4 h-[calc(100vh-16rem)]">{children}</div>}
+        <div className="w-full h-[250px] mt-4">
+        {children && React.isValidElement(children) ? (
+            <ResponsiveContainer width="100%" height="100%">
+              {children}
+            </ResponsiveContainer>
+          ) : (
+            <div>No chart data available</div>
+          )}
+        </div>
     </div>
   );
 };
