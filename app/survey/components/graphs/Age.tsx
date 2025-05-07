ddf9b1f72a4b05fee34d26fc116107c90dccaf46
zzhhaa <zzhhaa@protonmail.com>
2025-05-07 14:52:23 +0100

    feat: age pie chart

--- app/survey/components/graphs/Age.tsx
+++ app/survey/components/graphs/Age.tsx
@@ -1,9 +1,15 @@
 import React from "react"
 import { SurveyComponentProps } from "@/app/survey/types";
 import SurveyGraphCard from "@/app/survey/components/SurveyGraphCard";
+import { PieChart, Pie, Legend } from "recharts";
 
 export const Age: React.FC<SurveyComponentProps> = ({ results }) => {
     return (
-        <SurveyGraphCard title="How old are you?" results={results}></SurveyGraphCard>
+        <SurveyGraphCard title="How old are you?" results={results}>
+            <PieChart>
+                <Pie data={results.age} dataKey="value" nameKey="name" fill="#8884d8" />
+                <Legend align="center" verticalAlign="bottom"/>
+            </PieChart>
+        </SurveyGraphCard>
     )
 }
