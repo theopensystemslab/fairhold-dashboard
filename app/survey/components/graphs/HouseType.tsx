af487ad1bb76871278949a000e010a98efbd3118
zzhhaa <zzhhaa@protonmail.com>
2025-05-07 16:50:33 +0100

    feat: houseType sankey

--- app/survey/components/graphs/HouseType.tsx
+++ app/survey/components/graphs/HouseType.tsx
@@ -1,9 +1,24 @@
 import React from "react"
 import { SurveyComponentProps } from "@/app/survey/types";
 import SurveyGraphCard from "@/app/survey/components/SurveyGraphCard";
+import { Sankey, Tooltip } from "recharts"; 
 
 export const HouseType: React.FC<SurveyComponentProps> = ({ results }) => {
     return (
-        <SurveyGraphCard title="What type of home do you want to live in?" results={results}></SurveyGraphCard>
+        <SurveyGraphCard title="What type of home do you want to live in?" results={results}>
+            <Sankey
+                data={results.houseType}
+                node={{stroke: "#77c878", strokeWidth: 2}}
+                nodePadding={50}
+                margin={{
+                    left: 50,
+                     right: 50,
+                     top: 50,
+                     bottom: 50,
+                   }}
+                >
+                    <Tooltip />
+                </Sankey>
+        </SurveyGraphCard>
     )
 }
