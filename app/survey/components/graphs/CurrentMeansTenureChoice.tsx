7dbef62dbf8b07adcd28c5dd2cd33d7320565177
zzhhaa <zzhhaa@protonmail.com>
2025-05-08 12:48:21 +0100

    feat: CurrentMeansTenureChoice sankey

--- app/survey/components/graphs/CurrentMeansTenureChoice.tsx
+++ app/survey/components/graphs/CurrentMeansTenureChoice.tsx
@@ -1,9 +1,24 @@
 import React from "react"
 import { SurveyComponentProps } from "@/app/survey/types";
 import SurveyGraphCard from "@/app/survey/components/SurveyGraphCard";
+import { Sankey, Tooltip } from "recharts"; 
 
 export const CurrentMeansTenureChoice: React.FC<SurveyComponentProps> = ({ results }) => {
     return (
-        <SurveyGraphCard title="Could you afford a Fairhold home in your area?" results={results}></SurveyGraphCard>
+        <SurveyGraphCard title="With your current means, what tenure would you choose?" results={results}>
+            <Sankey
+                data={results.currentMeansTenureChoice}
+                node={{stroke: "#77c878", strokeWidth: 2}}
+                nodePadding={50}
+                margin={{
+                    left: 50,
+                        right: 50,
+                        top: 50,
+                        bottom: 50,
+                    }}
+                >
+                    <Tooltip />
+            </Sankey>
+        </SurveyGraphCard>
     )
 }
