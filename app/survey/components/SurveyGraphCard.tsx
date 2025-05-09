e31cd1e1e1be38ad73fbbaf2c7e711a69700a8c2
zzhhaa <zzhhaa@protonmail.com>
2025-05-09 11:19:28 +0100

    feat: add ResponsiveContainer to SurveyGraphCard

--- app/survey/components/SurveyGraphCard.tsx
+++ app/survey/components/SurveyGraphCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SurveyResults } from "@/app/survey/types";
+import { ResponsiveContainer } from "recharts";
 
 type Props = React.PropsWithChildren<{
   title: string;
@@ -12,8 +13,16 @@ const SurveyGraphCard: React.FC<Props> = ({ title, subtitle, children }) => {
     <div className="justify-center w-full bg-white m-4 p-4">
         <h3 className={`text-xl md:text-lg sm:text-md font-normal text-black`}>{title}</h3>
         {subtitle && <p className={`text-md md:text-lg text-gray-600 mt-2 font-normal`}>{subtitle}</p>}
-      {children && <div className="mt-4 h-[calc(100vh-16rem)]">{children}</div>}
-    </div>
+        <div className="w-full h-[250px] mt-4">
+        {children && React.isValidElement(children) ? (
+            <ResponsiveContainer width="100%" height="100%">
+              {children}
+            </ResponsiveContainer>
+          ) : (
+            <div>No chart data available</div>
+          )}
+        </div>
+      </div>
   );
 };
 
