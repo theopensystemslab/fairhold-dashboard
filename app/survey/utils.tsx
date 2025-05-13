4c2c9bc28c75669129114a06afcb31aea6cbefe1
zzhhaa <zzhhaa@protonmail.com>
2025-05-13 09:52:35 +0100

    refactor: use Object.entries instead

--- app/survey/utils.tsx
+++ app/survey/utils.tsx
@@ -5,17 +5,17 @@ export const aggregateResults = (rawResults: RawResults[]) => {
     const numberResponses = rawResults.length;
 
     for (const rawResult of rawResults) {
-        for (const key in rawResult) {
-            if (key === "id") continue; // Skip the id field (we don't need it)
+        Object.entries(rawResult).forEach(([key, value]) => {
+            if (key === "id") return; // Skip the id field (we don't need it)
+
             const validKey = key as keyof RawResults; // Explicitly assert key type
-            const answer = rawResult[validKey];
             if (!results[validKey]) {
                 results[validKey] = [];
             }
 
-            if (Array.isArray(answer)) {
+            if (Array.isArray(value)) {
                 // We need to handle arrays because some questions are multiple choice
-                for (const item of answer) {
+                for (const item of value) {
                     const existingResult = results[validKey].find((result) => result.answer === item);
                     if (existingResult) {
                         existingResult.value++;
@@ -25,14 +25,14 @@ export const aggregateResults = (rawResults: RawResults[]) => {
                 }
             } else {
                 // The rest of the answers are single strings
-                const existingResult = results[validKey].find((result) => result.answer === answer);
+                const existingResult = results[validKey].find((result) => result.answer === value);
                 if (existingResult) {
                     existingResult.value++;
                 } else {
-                    results[validKey].push({ answer: answer, value: 1 });
+                    results[validKey].push({ answer: value, value: 1 });
                 }
             }
-        }
+        })
     }
     return { numberResponses, results };
 }
