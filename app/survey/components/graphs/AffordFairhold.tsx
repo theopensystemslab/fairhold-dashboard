2c42fbdc2885ec344cf9ebd48e315278559d07f0
zzhhaa <zzhhaa@protonmail.com>
2025-05-14 15:05:44 +0100

    feat: housing preferences bar and pie charts

--- app/survey/components/graphs/AffordFairhold.tsx
+++ app/survey/components/graphs/AffordFairhold.tsx
@@ -1,9 +1,17 @@
 import React from "react"
 import { BarOrPieResults } from "@/app/survey/types";
 import SurveyGraphCard from "@/app/survey/components/SurveyGraphCard";
+import { PieChart, Pie, Legend, ResponsiveContainer } from "recharts";
 
-export const AffordFairhold: React.FC<BarOrPieResults> = ( results ) => {
+export const AffordFairhold: React.FC<BarOrPieResults> = ({ affordFairhold }) => {
     return (
-        <SurveyGraphCard title="Could you afford a Fairhold home in your area?" results={results}></SurveyGraphCard>
+        <SurveyGraphCard title="Could you afford a Fairhold home in your area?">
+            <ResponsiveContainer width="100%" height="100%">
+                <PieChart>
+                    <Pie data={affordFairhold} dataKey="value" nameKey="answer" fill="rgb(var(--survey-placeholder))" />
+                    <Legend align="center" verticalAlign="bottom" />
+                </PieChart>
+            </ResponsiveContainer>
+        </SurveyGraphCard>
     )
 }
