be71acedb21a142f4a24e6ad89c748efa0806990
zzhhaa <zzhhaa@protonmail.com>
2025-05-14 14:55:43 +0100

    feat: render CustomSankeys in graphs

--- app/survey/components/graphs/AnyMeansTenureChoice.tsx
+++ app/survey/components/graphs/AnyMeansTenureChoice.tsx
@@ -1,9 +1,18 @@
 import React from "react"
 import { SankeyResults } from "@/app/survey/types";
 import SurveyGraphCard from "@/app/survey/components/SurveyGraphCard";
+import { CustomSankey } from "../CustomSankey";
+import { ResponsiveContainer } from "recharts";
 
-export const AnyMeansTenureChoice: React.FC<SankeyResults> = (results) => {
+export const AnyMeansTenureChoice: React.FC<SankeyResults> = ({ anyMeansTenureChoice }) => {
     return (
-        <SurveyGraphCard title="What tenure would you choose?" results={results}></SurveyGraphCard>
+        <SurveyGraphCard title="What tenure would you choose?">
+            <ResponsiveContainer>
+                <CustomSankey
+                    nodes={anyMeansTenureChoice.nodes}
+                    links={anyMeansTenureChoice.links}            >
+                </CustomSankey>
+            </ResponsiveContainer>
+        </SurveyGraphCard>
     )
 }
--- app/survey/components/graphs/LiveWith.tsx
+++ app/survey/components/graphs/LiveWith.tsx
@@ -1,9 +1,18 @@
 import React from "react"
 import { SankeyResults } from "@/app/survey/types";
 import SurveyGraphCard from "@/app/survey/components/SurveyGraphCard";
+import { CustomSankey } from "../CustomSankey"
+import { ResponsiveContainer } from "recharts";
 
-export const LiveWith: React.FC<SankeyResults> = (results) => {
+export const LiveWith: React.FC<SankeyResults> = ({ liveWith }) => {
     return (
-        <SurveyGraphCard title="Who do you want to live with?" results={results}></SurveyGraphCard>
+        <SurveyGraphCard title="Who do you want to live with?">
+            <ResponsiveContainer>
+            <CustomSankey
+                nodes={liveWith.nodes}
+                links={liveWith.links}            >
+            </CustomSankey>
+            </ResponsiveContainer>
+        </SurveyGraphCard>
     )
 }
