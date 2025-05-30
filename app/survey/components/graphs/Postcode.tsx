3925ab20a840419a2544dec36b6320db88ca9cad
zzhhaa <zzhhaa@protonmail.com>
2025-05-30 11:30:35 +0100

    nit: remove console log

--- app/survey/components/graphs/Postcode.tsx
+++ app/survey/components/graphs/Postcode.tsx
@@ -3,7 +3,8 @@ import { BarOrPieResults } from "@/app/survey/types";
 import SurveyGraphCard from "@/app/survey/components/SurveyGraphCard";
 
 export const Postcode: React.FC<BarOrPieResults> = ({ postcode }) => {
-    console.log(postcode)
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const placeholder = postcode;
     return (
         <SurveyGraphCard title="What is your postcode?"></SurveyGraphCard>
     )
