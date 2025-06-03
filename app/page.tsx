db7d8e34bc2218e87cdd667d3bb9de72ab6d7e37
zzhhaa <zzhhaa@protonmail.com>
2025-06-03 11:46:42 +0100

    feat: route results to new page

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,35 @@
+"use client"
+import React from "react";
 import CalculatorInput from "./components/ui/CalculatorInput";
-import { ClipLoader } from "react-spinners";
-import { Suspense } from "react";
 import { Inter } from "next/font/google";
+import { Header } from "./components/ui/Header";
 import { Footer } from "./components/ui/Footer";
+import { FormFrontend } from "@/app/schemas/formSchema";
+import { useRouter } from "next/navigation";
 
 const inter = Inter({
   weight: ["500", "600", "700", "800"],
   subsets: ["greek", "greek-ext", "latin", "latin-ext"],
 });
 
-const Header = () => (
-  <header className="static top-0 left-0 bg-transparent flex items-center px-4 py-4 z-50 height-10vh">
-    <h1 className="font-bold text-xl text-black">Fairhold</h1>
-  </header>
-);
+
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleSubmit = (formData: FormFrontend) => {
+    const params = new URLSearchParams();
+    Object.entries(formData).forEach(([key, value]) => {
+      if (value !== undefined && value !== null) params.append(key, String(value));
+    });
+    router.push(`/results?${params.toString()}`);
+  };
+
   return (
     <main className={inter.className}>
       <Header />
-      <Suspense fallback={<ClipLoader />}>
-        <CalculatorInput />
-        <Footer/>
-      </Suspense>
+      <CalculatorInput onSubmit={handleSubmit} isLoading={false} />
+      <Footer />
     </main>
   );
 }
