8bef22a42242fd841a83f8190fa78db040f3eecd
zzhhaa <zzhhaa@protonmail.com>
2025-06-24 11:00:31 +0100

    style: SupportDevelopment colours, order and donut ring

--- app/survey/constants.tsx
+++ app/survey/constants.tsx
@@ -3,3 +3,12 @@ export const AFFORD_FAIRHOLD_ORDER = [
   "Yes, but Fairhold Land Rent only, because the deposit is lower",
   "No, it's still too expensive",
 ];
+
+export const SUPPORT_DEVELOPMENT_ORDER = [
+    "Strongly supportive of any development", 
+    "Quite supportive of any development", 
+    "It depends", 
+    "Quite opposed to most development",
+    "Strongly opposed to any development",
+    "Don't know",
+]
