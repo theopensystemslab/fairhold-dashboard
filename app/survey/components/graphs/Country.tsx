037bcfdaa65d02a08cf11f92355077a946fca4b7
zzhhaa <zzhhaa@protonmail.com>
2025-06-24 10:41:39 +0100

    style: Country colours and donut ring

--- app/survey/components/graphs/Country.tsx
+++ app/survey/components/graphs/Country.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import SurveyGraphCard from "@/app/survey/components/SurveyGraphCard";
-import { PieChart, Pie, Legend, ResponsiveContainer } from "recharts";
+import { PieChart, Pie, Legend, ResponsiveContainer, Cell } from "recharts";
 import { useSurveyContext } from "../../context";
 
   export const Country = () => {
-    const uk = useSurveyContext().barOrPie.uk;
+    const { uk } = useSurveyContext().barOrPie;
     
+   const COLORS = [
+      "rgb(var(--survey-blue))", "rgb(var(--survey-blue-light))"
+    ];
+  
     return (
       <SurveyGraphCard title="Which country?">
         <ResponsiveContainer width="100%" height="100%">
@@ -15,9 +19,13 @@ import { useSurveyContext } from "../../context";
               dataKey="value" 
               nameKey="answer" 
               fill="rgb(var(--survey-placeholder))" 
-              innerRadius="40%"
+              innerRadius="60%"
               outerRadius="80%"
-              />
+              >
+              {uk.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))}
+              </Pie>
             <Legend align="center" verticalAlign="bottom" />
             </PieChart>
         </ResponsiveContainer>
