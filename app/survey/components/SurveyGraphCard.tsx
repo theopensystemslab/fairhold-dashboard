a3a94d932f76869a305a293566e6b00833242e6c
zzhhaa <zzhhaa@protonmail.com>
2025-06-24 13:50:57 +0100

    style: adjust SurveyGraphCard subtitle size

--- app/survey/components/SurveyGraphCard.tsx
+++ app/survey/components/SurveyGraphCard.tsx
@@ -9,7 +9,7 @@ const SurveyGraphCard: React.FC<Props> = ({ title, subtitle, children }) => {
   return (
     <div className="justify-center w-full bg-white m-4 p-4">
         <h3 className={`text-xl md:text-lg sm:text-md font-bold text-black`} dangerouslySetInnerHTML={{ __html: title }}/>
-        {subtitle && <p className={`text-md md:text-lg text-gray-600 mt-2 font-normal`}>{subtitle}</p>}
+        {subtitle && <p className={`text-md text-gray-600 mt-2 font-normal`}>{subtitle}</p>}
       {children && <div className="mt-4 h-[calc(60vh-16rem)]">{children}</div>}
     </div>
   );
