3d498e8fc37514a587a334f1de3be8249a9a98dc
zzhhaa <zzhhaa@protonmail.com>
2025-06-24 10:53:44 +0100

    style: AffordFairhold colours, order and donut ring

--- app/survey/components/graphs/AffordFairhold.tsx
+++ app/survey/components/graphs/AffordFairhold.tsx
@@ -1,9 +1,21 @@
 import React from "react"
 import SurveyGraphCard from "@/app/survey/components/SurveyGraphCard";
-import { PieChart, Pie, Legend, ResponsiveContainer } from "recharts";
+import { PieChart, Pie, Legend, ResponsiveContainer, Cell } from "recharts";
 import { useSurveyContext } from "../../context";
+import { AFFORD_FAIRHOLD_ORDER } from "../../constants";
+
 export const AffordFairhold = () => {
-    const affordFairhold = useSurveyContext().barOrPie.affordFairhold;
+    let { affordFairhold } = useSurveyContext().barOrPie;
+
+    affordFairhold = affordFairhold.slice().sort(
+        (a, b) =>
+            AFFORD_FAIRHOLD_ORDER.indexOf(a.answer as string) -
+            AFFORD_FAIRHOLD_ORDER.indexOf(b.answer as string)
+        );
+
+    const COLORS = [
+      "rgb(var(--fairhold-equity-color-rgb))", "rgb(var(--fairhold-interest-color-rgb))", "rgb(var(--social-rent-land-color-rgb))" 
+    ];
 
     return (
         <SurveyGraphCard title="Could you afford a Fairhold home in your area?">
@@ -14,9 +26,13 @@ export const AffordFairhold = () => {
                         dataKey="value" 
                         nameKey="answer" 
                         fill="rgb(var(--survey-placeholder))" 
-                        innerRadius="40%"
+                        innerRadius="60%"
                         outerRadius="80%"
-                        />
+                        >
+                        {affordFairhold.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
                     <Legend align="center" verticalAlign="bottom" />
                 </PieChart>
             </ResponsiveContainer>
---
+++ app/survey/constants.tsx
@@ -0,0 +1,5 @@
+export const AFFORD_FAIRHOLD_ORDER = [
+  "Yes",
+  "Yes, but Fairhold Land Rent only, because the deposit is lower",
+  "No, it's still too expensive",
+];
