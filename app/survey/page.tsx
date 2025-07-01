91d819877724d929112618e9f59c82c1dcc9be93
zzhhaa <zzhhaa@protonmail.com>
2025-07-01 16:25:40 +0100

    fix: rebase style issue

--- app/survey/page.tsx
+++ app/survey/page.tsx
@@ -65,66 +65,75 @@ export default function SurveyPage() {
     <ErrorBoundary>
       <Header />
         <SurveyContext.Provider value={surveyResults}>
-          <main className={`${inter.className} min-h-screen bg-[rgb(var(--background-end-rgb))]`}>
-            <div className="flex flex-col m-4">
-              <h1 className="h1-style text-2xl md:text-4xl">Fairhold survey results</h1>
-                <div className="flex flex-col gap-4 mt-6">
-                {surveyResults.numberResponses === 0 ? (
-              <p>No survey responses found.</p>
-            ) : (
-              <div>
-                <h2 className="text-xl md:text-2xl">So far, {surveyResults.numberResponses} people have responded</h2>
-                <div className="flex flex-col py-4">
-                  <h3 className="text-xl font-medium">Who has responded?</h3>
-                  <div className="flex flex-col md:flex-row">
-                    <Country />
-                    <Age />
-                    {/* <Postcode {...results} /> */}
-                  </div>
-                </div>
-                  <div className="flex flex-col">
-                    <h3 className="text-xl font-medium">Housing preferences</h3>
-                    <div className="flex flex-col md:flex-row w-full h-240 p-4">
-                      <IdealHouseType />
-                      <IdealLiveWith />
-                    </div>
-                    <div className="flex flex-col md:flex-row h-240 p-4">
-                      <HousingOutcomes />
-                      <AffordFairhold />
-                    </div>
-                    <div className="flex flex-col md:flex-row h-240 p-4">
-                      <CurrentMeansTenureChoice />
-                    </div>
-                    <div className="flex flex-col md:flex-row h-240 p-4">
-                      <WhyFairhold />
-                      <WhyNotFairhold />
-                    </div>
-                    <div className="flex flex-col md:flex-row  h-240 p-4">
-                      <div className="md:w-1/2 w-full mr-4">
-                        <AnyMeansTenureChoice />
+          <main className={`${inter.className} min-h-screen w-full bg-[rgb(var(--background-end-rgb))]`}>
+            <div className="flex flex-row">
+              <div className="hidden md:block w-1/4"></div>
+
+              <div className="w-full md:w-3/4 flex-1 flex justify-center">
+                <div className="w-full max-w-1280 flex flex-col p-4">
+                  <h1 className="h1-style text-2xl md:text-4xl">Fairhold survey results</h1>
+                    <div className="flex flex-col gap-4 mt-6">
+                    {surveyResults.numberResponses === 0 ? (
+                  <p>No survey responses found.</p>
+                ) : (
+                  <div>
+                    <h2 className="text-xl md:text-2xl">So far, {surveyResults.numberResponses} people have responded</h2>
+                    
+                    <div className="flex flex-col py-4">
+                      <h3 className="text-xl font-medium">Who has responded?</h3>
+                      <div className="flex flex-col md:flex-row h-240 p-4">
+                        <Country />
+                        <Age />
+                        {/* <Postcode {...results} /> */}
                       </div>
-                      <div className="md:w-1/2 md:mr-4 hidden"></div>
-                    </div>
                     </div>
 
-                  <div className="flex flex-col">
-                    <h3 className="text-xl font-medium">Attitudes towards development</h3>
-                    <div className="flex flex-col md:flex-row w-full md:gap-8 p-4">
-                      <div className="flex flex-col md:w-1/2 w-full h-480">
-                        <SupportDevelopment />
-                        <SupportNewFairhold />
+                    <div className="flex flex-col">
+                      <h3 className="text-xl font-medium">Housing preferences</h3>
+                      <div className="flex flex-col md:flex-row w-full h-240 p-4">
+                        <IdealHouseType />
+                        <IdealLiveWith />
                       </div>
-                      <div className="flex flex-col md:flex-row md:w-1/2 w-full h-480">
-                        <SupportDevelopmentFactors />
+                      <div className="flex flex-col md:flex-row h-240 p-4">
+                        <HousingOutcomes />
+                        <AffordFairhold />
+                      </div>
+                      <div className="flex flex-col md:flex-row h-240 p-4">
+                        <CurrentMeansTenureChoice />
+                      </div>
+                      <div className="flex flex-col md:flex-row h-240 p-4">
+                        <WhyFairhold />
+                        <WhyNotFairhold />
+                      </div>
+                      <div className="flex flex-col md:flex-row  h-240 p-4">
+                        <div className="md:w-1/2 w-full mr-4">
+                          <AnyMeansTenureChoice />
+                        </div>
+                        <div className="md:w-1/2 md:mr-4 hidden"></div>
                       </div>
                     </div>
+
+                    <div className="flex flex-col">
+                      <h3 className="text-xl font-medium">Attitudes towards development</h3>
+                      <div className="flex flex-col md:flex-row w-full md:gap-8 p-4">
+                        <div className="flex flex-col md:w-1/2 w-full h-480">
+                          <SupportDevelopment />
+                          <SupportNewFairhold />
+                        </div>
+                        <div className="flex flex-col md:flex-row md:w-1/2 w-full h-480">
+                          <SupportDevelopmentFactors />
+                        </div>
+                      </div>
+                    </div>
+                </div>
+                )}
                   </div>
-              </div>
-              )}
                 </div>
               </div>
+            </div>
           </main>
         </SurveyContext.Provider>
-        <Footer />
+      <Footer />
     </ErrorBoundary>
-  )};
+  );
+}
