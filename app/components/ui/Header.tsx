a4e8cd4cd6252fa853c7e0325f2f97ff4d1dfb98
zzhhaa <zzhhaa@protonmail.com>
2025-07-01 10:58:37 +0100

    chore: move Header into components

---
+++ app/components/ui/Header.tsx
@@ -0,0 +1,6 @@
+
+export const Header = () => (
+  <header className="static top-0 left-0 bg-transparent flex items-center px-4 py-4 z-50 height-10vh">
+    <h1 className="font-bold text-xl text-black">Fairhold</h1>
+  </header>
+);
