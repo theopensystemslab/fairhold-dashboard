52ec291bb2abdcbb6ccad9bca660b6907816d007
zzhhaa <zzhhaa@protonmail.com>
2025-07-10 09:35:41 +0100

    feat: show housingOutcomes by currentTenure

---
+++ app/survey/components/SurveyTenureSelector.tsx
@@ -0,0 +1,29 @@
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select";
+
+interface SurveyTenureSelectorProps {
+  options: string[];
+  value: string;
+  onChange: (value: string) => void;
+  color: string;
+}
+
+const SurveyTenureSelector: React.FC<SurveyTenureSelectorProps> = ({ options, value, onChange, color }) => (
+  <div>
+    <label>
+      <Select value={value} onValueChange={onChange}>
+        <SelectTrigger className="w-[180px]" style={{ color, border: 'none' }}>
+          <SelectValue placeholder="Select tenure" />
+        </SelectTrigger>
+        <SelectContent>
+          {options.map(option => (
+            <SelectItem key={option} value={option}>
+              {option}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </label>
+  </div>
+);
+
+export default SurveyTenureSelector;
