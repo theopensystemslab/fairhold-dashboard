25e52e8c244d6900683ba6e0cfe80a4f827b9b2a
zzhhaa <zzhhaa@protonmail.com>
2025-07-10 09:38:44 +0100

    feat: pass action to SurveyGraphCard

--- app/survey/components/SurveyGraphCard.tsx
+++ app/survey/components/SurveyGraphCard.tsx
@@ -3,14 +3,20 @@ import React from "react";
 type Props = React.PropsWithChildren<{
   title: string;
   subtitle?: React.ReactNode;
+  action?: React.ReactNode;
 }>;
 
-const SurveyGraphCard: React.FC<Props> = ({ title, subtitle, children }) => {
+const SurveyGraphCard: React.FC<Props> = ({ title, subtitle, action, children }) => {
   return (
     <div className="justify-center w-full bg-white m-4 p-4">
         <h3 className={`text-xl md:text-lg sm:text-md font-bold text-black`}>{title}</h3>
-        {subtitle && <p className={`text-md md:text-lg text-gray-600 mt-2 font-normal`}>{subtitle}</p>}
-      {children && <div className="mt-4 h-[calc(60vh-16rem)]">{children}</div>}
+                {(subtitle || action) && (
+          <div className="flex items-center gap-4 mt-2">
+            {subtitle && <p className="text-md text-gray-600 font-normal">{subtitle}</p>}
+            {action}
+          </div>
+        )}      
+        {children && <div className="mt-4 h-[calc(60vh-16rem)]">{children}</div>}
     </div>
   );
 };
