865ecb7ef981ea23bc0f7cc89cb870c58d5f19fc
zzhhaa <zzhhaa@protonmail.com>
2025-08-05 12:30:27 +0100

    style: header to match webflow

--- components/custom/ui/Header.tsx
+++ components/custom/ui/Header.tsx
@@ -1,8 +1,20 @@
 
 export const Header = () => (
-  <header className="static top-0 left-0 bg-transparent flex items-center px-4 py-4 z-50 height-10vh"
+  <header className="static top-0 left-0 bg-transparent flex items-center justify-center px-4 py-4 z-50 max-h-[60px]"
       style={{ background: "rgb(var(--background-start-rgb))" }}
   >
-    <h1 className="font-bold text-xl text-black">Fairhold</h1>
+    <section className="w-full max-w-[1280px] flex flex-row py-8">
+      <div className="w-1/4 flex items-center">
+        <a href="#" className="brand w-nav-brand"><div className="logo">Fairhold</div></a>
+      </div>
+      <div className="w-3/4">
+        <nav className="flex flex-row justify-end items-center space-x-0 w-nav-menu">
+          <a href="#" className="nav-link w-nav-link">Home</a>
+          <a href="#" className="nav-link w-nav-link">Explore</a>
+          <a href="#" className="nav-link w-nav-link">Take the survey</a>
+          <a href="#" className="nav-link w-nav-link">Contact</a>
+        </nav>
+      </div>
+    </section>
   </header>
 );
