b2742fbb8c3bd4329851ebef72b18dbb2683a625
zzhhaa <zzhhaa@protonmail.com>
2025-08-08 11:27:32 +0100

    chore: replace svg with radix Cross1Icon

--- components/custom/ui/Header.tsx
+++ components/custom/ui/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { HamburgerMenuIcon } from "@radix-ui/react-icons"
+import { HamburgerMenuIcon, Cross1Icon } from "@radix-ui/react-icons"
 
 export const Header = () => {
   return (
@@ -49,9 +49,7 @@ const MobileHeader = () => {
         <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex flex-col md:hidden">
           <div className="bg-white w-2/3 h-full p-8 flex flex-col ml-auto">
             <button className="mb-8 self-end" onClick={() => setMenuOpen(false)} aria-label="Close menu">
-              <svg width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2">
-                <path d="M6 6l12 12M6 18L18 6" />
-              </svg>
+              <Cross1Icon />
             </button>
             <nav className="flex flex-col flex-1 align-center">
               <ul className="flex flex-col space-y-4 items-center list-none m-0 p-0">
