d69a96827b5e6827eb005db37423e70df8845b09
zzhhaa <zzhhaa@protonmail.com>
2025-08-08 11:25:44 +0100

    chore: make menus ul elements

--- components/custom/ui/Header.tsx
+++ components/custom/ui/Header.tsx
@@ -23,7 +23,9 @@ const DesktopHeader = () => {
   return (
     <div className="hidden md:block">
       <nav className="flex flex-row justify-end items-center space-x-0 w-nav-menu">
-        <Links />
+        <ul className="flex flex-row space-x-6">
+          <Links />
+        </ul>
       </nav>
     </div>
   );
@@ -44,15 +46,17 @@ const MobileHeader = () => {
         </button>
       </div>
       {menuOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex flex-col items-end md:hidden">
-          <div className="bg-white w-2/3 h-full p-8">
-            <button className="mb-8" onClick={() => setMenuOpen(false)} aria-label="Close menu">
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex flex-col md:hidden">
+          <div className="bg-white w-2/3 h-full p-8 flex flex-col ml-auto">
+            <button className="mb-8 self-end" onClick={() => setMenuOpen(false)} aria-label="Close menu">
               <svg width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2">
                 <path d="M6 6l12 12M6 18L18 6" />
               </svg>
             </button>
-            <nav className="flex flex-col space-y-6">
-              <Links />
+            <nav className="flex flex-col flex-1 align-center">
+              <ul className="flex flex-col space-y-4 items-center list-none m-0 p-0">
+                <Links />
+              </ul>
             </nav>
           </div>
         </div>
@@ -64,10 +68,10 @@ const MobileHeader = () => {
 const Links = () => {
   return (
     <>
-        <a href="#" className="nav-link w-nav-link">Home</a>
-        <a href="#" className="nav-link w-nav-link">Explore</a>
-        <a href="#" className="nav-link w-nav-link">Take the survey</a>
-        <a href="#" className="nav-link w-nav-link">Contact</a>
+        <li><a href="#" className="nav-link w-nav-link">Home</a></li>
+        <li><a href="#" className="nav-link w-nav-link">Explore</a></li>
+        <li><a href="#" className="nav-link w-nav-link">Take the survey</a></li>
+        <li><a href="#" className="nav-link w-nav-link">Contact</a></li>
     </>
   )
 }
