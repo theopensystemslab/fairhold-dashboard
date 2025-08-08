cd8ba4a2bfc01e246bc35ed4adaa4fd033a89f1e
zzhhaa <zzhhaa@protonmail.com>
2025-08-08 09:54:43 +0100

    feat: use ReactFC on CustomTick

--- components/custom/survey/CustomTick.tsx
+++ components/custom/survey/CustomTick.tsx
@@ -1,22 +1,22 @@
 import { TickProps } from "@lib/survey/types";
 
-const CustomTick = (props: TickProps, color?: string) => {
-      const { x, y, payload } = props;
-      return (
-        <g transform={`translate(${x},${y})`}>
-          <text 
-            x={-10} 
-            y={0} 
-            dy={4} 
-            textAnchor="end" 
-            fill={color || "#333"} 
-            fontSize={12}
-            width={240}
-          >
-            {payload.value}
-          </text>
-        </g>
-      );
-  }
+interface Props extends TickProps {
+  color?: string;
+}
+const CustomTick: React.FC<Props> = ({ x, y, payload, color }) => (
+    <g transform={`translate(${x},${y})`}>
+      <text 
+        x={-10} 
+        y={0} 
+        dy={4} 
+        textAnchor="end" 
+        fill={color || "#333"} 
+        fontSize={12}
+        width={240}
+      >
+        {payload.value}
+      </text>
+    </g>
+  );
 
   export default CustomTick;
