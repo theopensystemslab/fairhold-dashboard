6253de46d3cb3d0a6e8af6c51ab11e2537cea573
zzhhaa <zzhhaa@protonmail.com>
2025-08-12 16:03:49 +0100

    feat: animate pie charts on scroll, not load

--- components/custom/survey/graphs/SupportNewFairhold.tsx
+++ components/custom/survey/graphs/SupportNewFairhold.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import SurveyGraphCard from "@/components/custom/survey/SurveyGraphCard";
 import { PieChart, Pie, Legend, ResponsiveContainer, Cell } from "recharts";
 import { useSurveyContext } from "@context/surveyContext";
-
+import { useAnimateOnView } from "@hooks/UseAnimateOnView";
 
 export const SupportNewFairhold = () => {
     const supportNewFairhold = useSurveyContext().barOrPie.supportNewFairhold;
@@ -11,29 +11,34 @@ export const SupportNewFairhold = () => {
       "rgb(var(--fairhold-equity-color-rgb))", "rgb(var(--fairhold-interest-color-rgb))", "rgb(var(--survey-orange))", "rgb(var(--survey-pink))", "rgb(var(--social-rent-land-color-rgb))", "rgb(var(--survey-grey-light))"
     ];
     
+    const { ref, animate } = useAnimateOnView();
+    
     return (
         <SurveyGraphCard title="Would you support the creation of new Fairhold homes (or plots) in your area?">
-             <ResponsiveContainer height={300}>
-                <PieChart>
-                    <Pie 
-                        data={supportNewFairhold} 
-                        dataKey="value" 
-                        nameKey="answer" 
-                        fill="rgb(var(--survey-placeholder))" 
-                        innerRadius="60%"
-                        outerRadius="80%"
-                        >
-                        {supportNewFairhold.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                        ))}
-                    </Pie>
-                    <Legend 
-                        align="left" 
-                        verticalAlign="bottom" 
-                        wrapperStyle={{ fontSize: 14 }}
-                    />
-             </PieChart>
-            </ResponsiveContainer>
+            <div ref={ref} style={{ width: "100%", height: "100%" }}>
+                <ResponsiveContainer height={300}>
+                    <PieChart>
+                        <Pie 
+                            data={supportNewFairhold} 
+                            dataKey="value" 
+                            nameKey="answer" 
+                            fill="rgb(var(--survey-placeholder))" 
+                            innerRadius="60%"
+                            outerRadius="80%"
+                            isAnimationActive={animate}
+                            >
+                            {supportNewFairhold.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                            ))}
+                        </Pie>
+                        <Legend 
+                            align="left" 
+                            verticalAlign="bottom" 
+                            wrapperStyle={{ fontSize: 14 }}
+                        />
+                </PieChart>
+                </ResponsiveContainer>
+            </div>
         </SurveyGraphCard>
     )
 }
