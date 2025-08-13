a76d758bed9f40e0d88f47c6ebbdeba6c22ef684
zzhhaa <zzhhaa@protonmail.com>
2025-08-13 10:33:23 +0100

    style: add animation delay time

--- hooks/UseAnimateOnView.tsx
+++ hooks/UseAnimateOnView.tsx
@@ -3,10 +3,11 @@ import { useInView } from "react-intersection-observer";
 /**
  * Returns boolean indicating if element is in view.
  */
-export function useAnimateOnView(threshold = 0.01) {
+export function useAnimateOnView(threshold = 0.01, delay = 0.5) {
   const { ref, inView } = useInView({
     triggerOnce: true,
     threshold,
+    delay
   });
   return { ref, animate: inView };
 }
