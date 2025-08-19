264141e4d8bc8579c88f404c01bf4967f4325706
zzhhaa <zzhhaa@protonmail.com>
2025-08-19 11:42:27 +0100

    refactor: move hook to shared SurveyGraphCard

--- components/custom/survey/graphs/Country.tsx
+++ components/custom/survey/graphs/Country.tsx
@@ -3,7 +3,6 @@ import SurveyGraphCard from "@/components/custom/survey/SurveyGraphCard";
 import { PieChart, Pie, Legend, ResponsiveContainer, Cell } from "recharts";
 import { useSurveyContext } from "@context/surveyContext";
 import { BarOrPieResult } from "@/lib/survey/types";
-import { useAnimateOnView } from "@hooks/UseAnimateOnView";
 
   export const Country = () => {
     const { uk } = useSurveyContext().barOrPie as { uk: BarOrPieResult[] };
@@ -11,35 +10,35 @@ import { useAnimateOnView } from "@hooks/UseAnimateOnView";
    const COLORS = [
       "rgb(var(--survey-blue))", "rgb(var(--survey-blue-light))"
     ];
-
-    const { ref, animate } = useAnimateOnView();
   
     return (
       <SurveyGraphCard title="Where do you live?">
-        <div ref={ref} style={{ width: "100%", height: "100%" }}>
-          <ResponsiveContainer width="100%" height="100%">
-              <PieChart>
-              <Pie 
-                data={uk} 
-                dataKey="value" 
-                nameKey="answer" 
-                fill="rgb(var(--survey-placeholder))" 
-                innerRadius="60%"
-                outerRadius="80%"
-                isAnimationActive={animate}
-                >
-                {uk.map((entry, index) => (
-                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                ))}
-                </Pie>
-              <Legend 
-                align="left" 
-                verticalAlign="bottom" 
-                wrapperStyle={{ fontSize: 14 }}
-              />
-              </PieChart>
-          </ResponsiveContainer>
-        </div>
+          {(animate) => (
+            <div style={{ width: "100%", height: "100%" }}>
+              <ResponsiveContainer width="100%" height="100%">
+                  <PieChart>
+                  <Pie 
+                    data={uk} 
+                    dataKey="value" 
+                    nameKey="answer" 
+                    fill="rgb(var(--survey-placeholder))" 
+                    innerRadius="60%"
+                    outerRadius="80%"
+                    isAnimationActive={animate}
+                    >
+                    {uk.map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                    </Pie>
+                  <Legend 
+                    align="left" 
+                    verticalAlign="bottom" 
+                    wrapperStyle={{ fontSize: 14 }}
+                  />
+                  </PieChart>
+              </ResponsiveContainer>
+            </div>
+          )}
       </SurveyGraphCard>
     );
   };
