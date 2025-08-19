b9843eb91d7034dcf5bea8de3b64475e16caa918
zzhhaa <zzhhaa@protonmail.com>
2025-08-19 11:30:10 +0100

    chore: move useAnimateOnView to SurveyGraphCard

--- components/custom/survey/SurveyGraphCard.tsx
+++ components/custom/survey/SurveyGraphCard.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { useAnimateOnView } from "@hooks/UseAnimateOnView";
 
-type Props = React.PropsWithChildren<{
+type Props = {
   title: string;
   subtitle?: React.ReactNode;
   action?: React.ReactNode;
-}>;
+  children?: (animate: boolean) => React.ReactNode;
+};
 
 const SurveyGraphCard: React.FC<Props> = ({ title, subtitle, action, children }) => {
+  const { ref, animate } = useAnimateOnView();
+
   return (
-    <div className="flex flex-1 flex-col justify-center h-full w-full bg-white p-4">
+    <div ref={ref} className="flex flex-1 flex-col justify-center h-full w-full bg-white p-4">
         <h3 className={`text-xl md:text-lg sm:text-md font-bold text-black mt-0`}>{title}</h3>
-                {(subtitle || action) && (
+        {(subtitle || action) && (
           <div className="flex items-center gap-4 mt-2">
             {subtitle && (
               <p className="text-md font-normal" style={{ color: "rgba(var(--survey-grey-mid))" }}>
@@ -20,7 +24,7 @@ const SurveyGraphCard: React.FC<Props> = ({ title, subtitle, action, children })
             {action}
           </div>
         )}      
-        {children && <div className="mt-4 flex-1">{children}</div>}
+      {typeof children === "function" ? children(animate) : children}
     </div>
   );
 };
