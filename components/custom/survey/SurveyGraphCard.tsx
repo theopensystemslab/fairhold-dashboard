31379037588da9ad6cd6206d31d169e1115e3ea6
zzhhaa <zzhhaa@protonmail.com>
2025-08-21 11:17:02 +0100

    refactor: move skeleton to SurveyGraphCard

--- components/custom/survey/SurveyGraphCard.tsx
+++ components/custom/survey/SurveyGraphCard.tsx
@@ -1,13 +1,14 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
-import { ClipLoader } from "react-spinners";
 
 type Props = React.PropsWithChildren<{
   title: string;
   subtitle?: React.ReactNode;
   action?: React.ReactNode;
+  loading?: boolean;
 }>;
 
-const SurveyGraphCard: React.FC<Props> = ({ title, subtitle, action, children }) => {
+const SurveyGraphCard: React.FC<Props> = ({ title, subtitle, action, children, loading }) => {
   return (
     <div className="flex flex-1 flex-col justify-center h-full w-full bg-white p-4">
         <h3 className={`text-xl md:text-lg sm:text-md font-bold text-black mt-0`}>{title}</h3>
@@ -22,13 +23,15 @@ const SurveyGraphCard: React.FC<Props> = ({ title, subtitle, action, children })
           </div>
         )}
         <div className="mt-4 flex-1">
-          {children ? (
-            children
-          ) : (
-            <div className="flex items-center justify-center h-full w-full">
-              <ClipLoader />
+          {loading ? (
+            <div className="w-full h-full flex flex-col items-center gap-4">
+              <Skeleton className="h-2/3 w-2/3 rounded-xl bg-gray-200 m-4" /> 
+              <div className="w-full m-4">
+                <Skeleton className="h-6 w-1/2 rounded-xl bg-gray-200" /> 
+              </div>
             </div>
-          )}
+          ) : children
+        }
         </div>
       </div>
   );
