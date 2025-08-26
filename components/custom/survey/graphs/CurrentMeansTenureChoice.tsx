aed9eef23932835eab3b7fea79531b09b6ccfd8f
zzhhaa <zzhhaa@protonmail.com>
2025-08-26 11:52:03 +0100

    feat: pass coloured sankey nodes to frontend

--- components/custom/survey/graphs/CurrentMeansTenureChoice.tsx
+++ components/custom/survey/graphs/CurrentMeansTenureChoice.tsx
@@ -3,17 +3,15 @@ import SurveyGraphCard from "@/components/custom/survey/SurveyGraphCard";
 import { CustomSankey } from "../CustomSankey"
 import { ResponsiveContainer } from "recharts";
 import { useSurveyContext } from "@context/surveyContext";
-import { applyNodeColors } from "@lib/survey/utils";
 
 export const CurrentMeansTenureChoice = () => {
     const { currentMeansTenureChoice } = useSurveyContext().sankey;
-    const colouredNodes = applyNodeColors(currentMeansTenureChoice.nodes);
 
     return (
         <SurveyGraphCard title="Which tenure would you choose?">
             <ResponsiveContainer width="100%" height="100%">
                 <CustomSankey
-                    nodes={colouredNodes}
+                    nodes={currentMeansTenureChoice.nodes}
                     links={currentMeansTenureChoice.links}     
                     leftLabel="I live in"
                     rightLabel="I want to live in"   
