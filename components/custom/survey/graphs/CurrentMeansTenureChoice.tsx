eb377dc7acb05f9cccf28a6deaaf49ee40ea26bd
zzhhaa <zzhhaa@protonmail.com>
2025-08-26 11:21:04 +0100

    chore: remove label mapping from graph cards

--- components/custom/survey/graphs/CurrentMeansTenureChoice.tsx
+++ components/custom/survey/graphs/CurrentMeansTenureChoice.tsx
@@ -5,32 +5,9 @@ import { ResponsiveContainer } from "recharts";
 import { useSurveyContext } from "@context/surveyContext";
 import { applyNodeColors } from "@lib/survey/utils";
 
-const getLabel = (name: string) => {
-    switch (name) {
-        case "Part own, part rent":
-            return "Shared ownership";
-        case "I live for free in a home":
-            return "Free housing";
-        case "I do not have a stable home at the moment":
-            return "No stable home";
-        default:
-            return name;
-    }
-};
-
 export const CurrentMeansTenureChoice = () => {
     const { currentMeansTenureChoice } = useSurveyContext().sankey;
-    
-    // We use suffixes to distinguish between current and ideal so we don't end up with circular links (eg for Freehold -> Freehold), so we need to remove the suffixes
-    const formattedNodes = currentMeansTenureChoice.nodes.map((node) => ({
-        ...node,
-        name: node.name.replace(/_(0|1)$/, ""),
-    }));
-    const labeledNodes = formattedNodes.map((node: { name: string }) => ({
-        ...node,
-        label: getLabel(node.name),
-    }));
-    const colouredNodes = applyNodeColors(labeledNodes);
+    const colouredNodes = applyNodeColors(currentMeansTenureChoice.nodes);
 
     return (
         <SurveyGraphCard title="Which tenure would you choose?">
--- components/custom/survey/graphs/IdealLiveWith.tsx
+++ components/custom/survey/graphs/IdealLiveWith.tsx
@@ -3,39 +3,15 @@ import SurveyGraphCard from "@/components/custom/survey/SurveyGraphCard";
 import { CustomSankey } from "../CustomSankey"
 import { useSurveyContext } from "@context/surveyContext";
 
-const getLabel = (name: string) => {
-    switch (name) {
-        case "I live with my parents":
-            return "Parents / extended family";
-        case "I live alone":
-            return "Alone";
-        case "I live with my parents or extended family": // intentional fallthrough
-        case "With my parents or extended family":
-            return "Parents / extended family";
-        case "I live with housemates":
-        case "With friends":
-            return "Friends / housemates";
-        case "I live with my partner / family":
-        case "With partner / family": // intentional fallthrough
-            return "Partner / family";
-        default:
-            return name;
-    }
-};
-
 export const IdealLiveWith = () => {
     const { idealLiveWith } = useSurveyContext().sankey;
     const color = "rgb(var(--fairhold-equity-color-rgb))";
     const coloredNodes = idealLiveWith.nodes.map((node) => ({...node, color}));
-    const labeledNodes = coloredNodes.map((node: { name: string }) => ({
-    ...node,
-    label: getLabel(node.name),
-    }));
 
     return (
         <SurveyGraphCard title="Who do you want to live with?">
             <CustomSankey
-                nodes={labeledNodes}
+                nodes={coloredNodes}
                 links={idealLiveWith.links}     
                 leftLabel="I live with"
                 rightLabel="I want to live with" 
