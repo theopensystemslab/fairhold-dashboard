abbb701232aab31745ca53664fe5309d69d532eb
zzhhaa <zzhhaa@protonmail.com>
2025-09-02 13:36:50 +0100

    style: adjust font colour and size

--- components/custom/ui/Footer.tsx
+++ components/custom/ui/Footer.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 
 export const Footer: React.FC = () => (
-  <footer className="bg-[rgb(var(--footer-background-rgb))] text-white py-8 ">
-    <div className="max-w-[1280px] mx-auto px-4">
+  <footer className="bg-[rgb(var(--footer-background-rgb))] text-white py-8 h-[375px]">
+    <div className="max-w-[1280px] mx-auto">
       {/* ROW 1 */}
       <div className="flex flex-col md:flex-row gap-8">
         {/* COL 1 */}
         <div className="flex flex-col md:w-1/4">
-          <a href="#" className="mb-1 text-sm hover:underline opacity-90">Terms of use</a>
-          <a href="#" className="mb-1 text-sm hover:underline opacity-90">Privacy</a>
-          <a href="#" className="mb-1 text-sm hover:underline opacity-90">Accessibility</a>
-          <a href="#" className="mb-1 text-sm hover:underline opacity-90">Contact us</a>
+          <a href="#" className="mb-1 text-base hover:underline opacity-90">Terms of use</a>
+          <a href="#" className="mb-1 text-base hover:underline opacity-90">Privacy</a>
+          <a href="#" className="mb-1 text-base hover:underline opacity-90">Accessibility</a>
+          <a href="#" className="mb-1 text-base hover:underline opacity-90">Contact us</a>
         </div>
         {/* COL 2 */}
         <div className="hidden md:w-1/4 md:block" />
@@ -41,7 +41,7 @@ export const Footer: React.FC = () => (
       <div className="flex flex-col md:flex-row gap-8">
         {/* COL 1 */}
         <div className="flex flex-col items-start md:w-1/4">
-          <span className="text-sm mb-8">Developed by</span>
+          <span className="text-base mb-8 opacity-90">Developed by</span>
           <a href="#" className="flex items-center gap-2 hover:opacity-60">
             <Image src="https://cdn.prod.website-files.com/665dd5e4ad0dc9aad281585c/6754893e21b56d4557aa3597_OSL_icon_white.svg"
               width={40} height={40} alt="OSL-logo" />
@@ -51,7 +51,7 @@ export const Footer: React.FC = () => (
 
         {/* COL 2 */}
         <div className="flex flex-col items-start md:w-1/4">
-          <span className="text-sm mb-8">Funded by</span>
+          <span className="text-base mb-8 opacity-90">Funded by</span>
           <div className="flex gap-6">
             <a href="#" className="hover:opacity-60">
               <Image src="https://cdn.prod.website-files.com/665dd5e4ad0dc9aad281585c/6754910ec9989275efe70268_TNCLFlogo.svg"
@@ -69,8 +69,8 @@ export const Footer: React.FC = () => (
 
         {/* COL 4 */}
         <div className="flex flex-col items-start md:w-1/4">
-          <span className="text-sm mb-2">© Open Systems Lab 2025</span>
-          <span className="text-sm">
+          <span className="text-base mb-2 opacity-90">© Open Systems Lab 2025</span>
+          <span className="text-base opacity-90">
             Open Systems Lab / OSL is a non-profit company 9152368<br />
             registered in England &amp; Wales
           </span>
